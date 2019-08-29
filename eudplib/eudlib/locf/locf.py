441c5e0f15727cfe848421f0496c0ea0ec85fd2f
armoha <kein0011@naver.com>
2019-08-29 20:54:11 +0900

    Location functions accepts 4 coordinates

--- eudplib/eudlib/locf/locf.py
+++ eudplib/eudlib/locf/locf.py
@@ -29,7 +29,7 @@ from ..memiof import f_posread_epd, f_setcurpl2cpcache, f_dwread_epd
 _loct = 0x58DC60
 
 
-def _locfgen(mod1, mod2, mod3, mod4, signed=False):
+def _locfgen2(mod1, mod2, mod3, mod4, signed=False):
 
     @c.EUDFunc
     def _locf(epd, x, y):
@@ -85,51 +85,134 @@ def _locfgen(mod1, mod2, mod3, mod4, signed=False):
     return _locf
 
 
-_SetLoc = _locfgen(c.SetTo, c.SetTo, c.SetTo, c.SetTo)
-_AddLoc = _locfgen(c.Add, c.Add, c.Add, c.Add)
-_DilateLoc = _locfgen(c.Add, c.Add, c.Add, c.Add, signed=True)
+def _locfgen4(mod1, mod2, mod3, mod4, signed=False):
+
+    @c.EUDFunc
+    def _locf(epd, l, t, r, b):
+        act = c.Forward()
+
+        c.VProc([epd, l], [
+            epd.AddNumber(ut.EPD(_loct)),
+            epd.SetDest(ut.EPD(act) + 4),
+        ] + [
+            l.SetDest(ut.EPD(act) + 5)
+            if not signed else [
+                c.SetMemory(act + 20, c.SetTo, ~0),
+                l.QueueSubtractTo(ut.EPD(act) + 5),
+            ]
+        ])
+        c.VProc([epd, t], [
+            [c.SetMemory(act + 20, c.Add, 1) if signed else []],
+            epd.AddNumber(1),
+            epd.AddDest(8),
+        ] + [
+            t.SetDest(ut.EPD(act) + 8 + 5)
+            if not signed else [
+                c.SetMemory(act + 52, c.SetTo, ~0),
+                t.QueueSubtractTo(ut.EPD(act) + 8 + 5),
+            ]
+        ])
+        c.VProc([epd, r], [
+            [c.SetMemory(act + 52, c.Add, 1) if signed else []],
+            epd.AddNumber(1),
+            epd.AddDest(8),
+            r.SetDest(ut.EPD(act) + 16 + 5)
+        ])
+        c.VProc([epd, b], [
+            epd.AddNumber(1),
+            epd.AddDest(8),
+            b.SetDest(ut.EPD(act) + 24 + 5)
+        ])
+        c.RawTrigger(
+            actions=[
+                act << c.SetMemory(0, mod1, 0),
+                c.SetMemory(0, mod2, 0),
+                c.SetMemory(0, mod3, 0),
+                c.SetMemory(0, mod4, 0),
+            ]
+        )
+
+    return _locf
 
 
-def f_setloc(locID, x, y):
+_SetLoc2 = _locfgen2(c.SetTo, c.SetTo, c.SetTo, c.SetTo)
+_AddLoc2 = _locfgen2(c.Add, c.Add, c.Add, c.Add)
+_DilateLoc2 = _locfgen2(c.Add, c.Add, c.Add, c.Add, signed=True)
+_SetLoc4 = _locfgen4(c.SetTo, c.SetTo, c.SetTo, c.SetTo)
+_AddLoc4 = _locfgen4(c.Add, c.Add, c.Add, c.Add)
+_DilateLoc4 = _locfgen4(c.Add, c.Add, c.Add, c.Add, signed=True)
+
+
+def f_setloc(locID, *coords):
+    if len(coords) != 2 and len(coords) != 4:
+        raise NotImplementedError("number of coordinates should be 2 or 4.")
     if isinstance(locID, str):
         locID = c.GetLocationIndex(locID)
-    if c.IsConstExpr(locID) and c.IsConstExpr(x) and c.IsConstExpr(y):
+    if c.IsConstExpr(locID) and all(c.IsConstExpr(c) for c in coords):
+        dst = _loct + 20 * locID
+        if len(coords) == 2:
+            l, t = coords
+            r, b = coords
+        else:
+            l, t, r, b = coords
         cs.DoActions([
-            c.SetMemory(_loct + 20 * locID, c.SetTo, x),
-            c.SetMemory(_loct + 20 * locID + 4, c.SetTo, y),
-            c.SetMemory(_loct + 20 * locID + 8, c.SetTo, x),
-            c.SetMemory(_loct + 20 * locID + 12, c.SetTo, y),
+            c.SetMemory(dst, c.SetTo, l),
+            c.SetMemory(dst + 4, c.SetTo, t),
+            c.SetMemory(dst + 8, c.SetTo, r),
+            c.SetMemory(dst + 12, c.SetTo, b),
         ])
+    elif len(coords) == 2:
+        _SetLoc2(locID * 5, *coords)
     else:
-        _SetLoc(locID * 5, x, y)
+        _SetLoc4(locID * 5, *coords)
 
 
-def f_addloc(locID, x, y):
+def f_addloc(locID, *coords):
+    if len(coords) != 2 and len(coords) != 4:
+        raise NotImplementedError("number of coordinates should be 2 or 4.")
     if isinstance(locID, str):
         locID = c.GetLocationIndex(locID)
-    if c.IsConstExpr(locID) and c.IsConstExpr(x) and c.IsConstExpr(y):
+    if c.IsConstExpr(locID) and all(c.IsConstExpr(c) for c in coords):
+        dst = _loct + 20 * locID
+        if len(coords) == 2:
+            l, t = coords
+            r, b = coords
+        else:
+            l, t, r, b = coords
         cs.DoActions([
-            c.SetMemory(_loct + 20 * locID, c.Add, x),
-            c.SetMemory(_loct + 20 * locID + 4, c.Add, y),
-            c.SetMemory(_loct + 20 * locID + 8, c.Add, x),
-            c.SetMemory(_loct + 20 * locID + 12, c.Add, y),
+            c.SetMemory(dst, c.Add, l),
+            c.SetMemory(dst + 4, c.Add, t),
+            c.SetMemory(dst + 8, c.Add, r),
+            c.SetMemory(dst + 12, c.Add, b),
         ])
+    elif len(coords) == 2:
+        _AddLoc2(locID * 5, *coords)
     else:
-        _AddLoc(locID * 5, x, y)
+        _AddLoc4(locID * 5, *coords)
 
 
-def f_dilateloc(locID, x, y):
+def f_dilateloc(locID, *coords):
+    if len(coords) != 2 and len(coords) != 4:
+        raise NotImplementedError("number of coordinates should be 2 or 4.")
     if isinstance(locID, str):
         locID = c.GetLocationIndex(locID)
-    if c.IsConstExpr(locID) and c.IsConstExpr(x) and c.IsConstExpr(y):
+    if c.IsConstExpr(locID) and all(c.IsConstExpr(c) for c in coords):
+        dst = _loct + 20 * locID
+        if len(coords) == 2:
+            l, t = coords
+            r, b = coords
+        else:
+            l, t, r, b = coords
         cs.DoActions([
-            c.SetMemory(_loct + 20 * locID, c.Subtract, x),
-            c.SetMemory(_loct + 20 * locID + 4, c.Subtract, y),
-            c.SetMemory(_loct + 20 * locID + 8, c.Add, x),
-            c.SetMemory(_loct + 20 * locID + 12, c.Add, y),
+            c.SetMemory(dst, c.Add, -l),
+            c.SetMemory(dst + 4, c.Add, -t),
+            c.SetMemory(dst + 8, c.Add, r),
+            c.SetMemory(dst + 12, c.Add, b),
         ])
+    elif len(coords) == 2:
+        _DilateLoc2(locID * 5, *coords)
     else:
-        _DilateLoc(locID * 5, x, y)
+        _DilateLoc4(locID * 5, *coords)
 
 
 @c.EUDFunc
