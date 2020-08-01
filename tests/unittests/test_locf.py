08b67cef752d9e6ca0f63223165ad9f911e5fd28
armoha <kein0011@naver.com>
2020-08-01 18:47:23 +0900

    Fix locfunc unittest

--- tests/unittests/test_locf.py
+++ tests/unittests/test_locf.py
@@ -3,7 +3,7 @@ from helper import *
 
 @TestInstance
 def test_locf():
-    loc = EUDVariable(0)
+    loc = EUDVariable(1)
 
     f_setloc(loc, 32, 64)
     f_addloc(loc, 16, 8)
@@ -11,17 +11,14 @@ def test_locf():
 
     L, T = f_getlocTL(loc)
 
+    test_equality("f_getlocTL test", [L, T], [44, 70])
     test_equality(
-        "f_getlocTL test", [L, T], [44, 70]
+        "Location functions test",
+        [f_dwread_epd(EPD(0x58DC68)), f_dwread_epd(EPD(0x58DC6C)),],
+        [52, 74],
     )
-    test_equality("Location functions test", [
-        f_dwread_epd(EPD(0x58DC68)),
-        f_dwread_epd(EPD(0x58DC6C)),
-    ], [52, 74])
 
     Pos = Db(i2b4(0x6FE05BD))
     f_setloc_epd(loc, EPD(Pos))
     L, T = f_getlocTL(loc)
-    test_equality(
-        "f_setloc_epd test", [L, T], [0x5BD, 0x6FE]
-    )
+    test_equality("f_setloc_epd test", [L, T], [0x5BD, 0x6FE])
