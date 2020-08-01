93dd3b7cdec6d1cdf2a388bf7e213feb37e37a6f
armoha <kein0011@naver.com>
2020-08-01 22:05:25 +0900

    Optimize UNIT, UNIx, UPGx, TECx

--- eudplib/core/mapdata/fixmapdata.py
+++ eudplib/core/mapdata/fixmapdata.py
@@ -26,6 +26,7 @@ THE SOFTWARE.
 
 def FixMapData(chkt):
     FixUnitMap(chkt)
+    FixSettings(chkt)
     ApplyRemasteredChk(chkt)
     RemoveLocationStringInfo(chkt)
 
@@ -34,12 +35,58 @@ def FixUnitMap(chkt):
     unit = bytearray(chkt.getsection("UNIT"))
 
     for i in range(0, len(unit), 36):
+        # Disable flags for default value
         if unit[i + 17] == 100:
             unit[i + 14] &= ~2
         if unit[i + 18] == 100:
             unit[i + 14] &= ~4
+        if unit[i + 24 : i + 26] == b"\0\0":
+            unit[i + 14] &= ~32
+        for k in range(5):
+            if unit[i + 26] & (2 ** k) == 0:
+                unit[i + 12] &= ~(2 ** k)
+
+        # Remove disabled values
+        if unit[i + 14] & 1 == 0:
+            unit[i + 16] = 0
+        if unit[i + 14] & 2 == 0:
+            unit[i + 17] = 0
+        if unit[i + 14] & 4 == 0:
+            unit[i + 18] = 0
+        if unit[i + 14] & 8 == 0:
+            unit[i + 19] = 0
+        if unit[i + 14] & 16 == 0:
+            unit[i + 20 : i + 24] = b"\0\0\0\0"
+        if unit[i + 14] & 32 == 0:
+            unit[i + 24 : i + 26] = b"\0\0"
+        for k in range(5):
+            if unit[i + 12] & (2 ** k) == 0:
+                unit[i + 26] &= ~(2 ** k)
 
     chkt.setsection("UNIT", unit)
+
+
+def FixSettings(chkt):
+    sections = (
+        ("UNIx", 228, (4, 2, 1, 2, 2, 2, 2)),
+        ("UPGx", 61, (2, 2, 2, 2, 2, 2)),
+        ("TECx", 44, (2, 2, 2, 2)),
+    )
+    for name, count, settings in sections:
+        data = bytearray(chkt.getsection(name))
+
+        for i in range(count):
+            if data[i] == 0:
+                continue
+            offset = count
+            if name == "UPGx":
+                offset += 1
+            for setting in settings:
+                for k in range(setting):
+                    data[offset + setting * i + k] = 0
+                offset += setting * count
+
+        chkt.setsection(name, data)
 
 
 def ApplyRemasteredChk(chkt):
