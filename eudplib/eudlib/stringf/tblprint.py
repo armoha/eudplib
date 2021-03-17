f1e4ae51fbb9f06dd7653991cef8dcc40d417198
armoha <kein0011@naver.com>
2021-03-17 14:08:15 +0900

    f_settbl supports UTF-8 encoding

--- eudplib/eudlib/stringf/tblprint.py
+++ eudplib/eudlib/stringf/tblprint.py
@@ -29,25 +29,30 @@ from .eudprint import f_dbstr_print
 from .fmtprint import f_sprintf
 
 
-def f_settbl(tblID, offset, *args):
+def f_settbl(tblID, offset, *args, encoding="cp949"):
     dst = GetTBLAddr(tblID)
     dst += offset
-    f_dbstr_print(dst, *args, encoding="cp949")
+    if encoding.casefold() == "UTF-8".casefold():
+        f_dbstr_print(dst, *args, "\u2009", encoding="UTF-8")
+    else:
+        f_dbstr_print(dst, *args, encoding=encoding)
 
 
-def f_settbl2(tblID, offset, *args):
+def f_settbl2(tblID, offset, *args, encoding="cp949"):
     dst = GetTBLAddr(tblID)
     dst += offset
-    f_dbstr_print(dst, *args, EOS=False, encoding="cp949")
+    f_dbstr_print(dst, *args, EOS=False, encoding=encoding)
 
 
-def f_settblf(tblID, offset, format_string, *args):
+def f_settblf(tblID, offset, format_string, *args, encoding="cp949"):
     dst = GetTBLAddr(tblID)
     dst += offset
-    f_sprintf(dst, format_string, *args, encoding="cp949")
+    if encoding.casefold() == "UTF-8".casefold() and format_string[-1] != "\u2009":
+        format_string += "\u2009"
+    f_sprintf(dst, format_string, *args, encoding=encoding)
 
 
-def f_settblf2(tblID, offset, format_string, *args):
+def f_settblf2(tblID, offset, format_string, *args, encoding="cp949"):
     dst = GetTBLAddr(tblID)
     dst += offset
-    f_sprintf(dst, format_string, *args, EOS=False, encoding="cp949")
+    f_sprintf(dst, format_string, *args, EOS=False, encoding=encoding)
