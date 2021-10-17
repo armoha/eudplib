6657f85fb1583ad3c4bcede4269d3ead1508d6a1
armoha <kein0011@naver.com>
2021-10-18 04:14:49 +0900

    Clean PlayerName code

--- eudplib/eudlib/stringf/pname.py
+++ eudplib/eudlib/stringf/pname.py
@@ -138,188 +138,195 @@ def IsPName(player, name):
     return c.DeathsX(p, *params)
 
 
-isPNameInitialized = False
+class _PlayerName:
+    def __init__(self):
+        self.is_chatptr_unchanged = c.EUDLightBool()
+        self.db = c.Db(218)
+        self.ptr, self.epd = c.EUDCreateVariables(2)
+        self.odds_or_even = c.EUDLightBool()
+        self.name_db_list = [c.Db(26) for _ in range(8)]
+        self.basenames, self.baselens = PVariable(self.name_db_list), PVariable()
+        self.optimize_start, self.optimize_end = c.Forward(), c.Forward()
+        self.odd = [c.Forward() for p in range(8)]
+        self.even0 = [c.Forward() for p in range(8)]
+        self.even1 = [c.Forward() for p in range(8)]
 
-isTxtPtrUnchanged = None
-temp_Db = None
-ptr, epd = None, None
-odds_or_even = None
-baselens = None
-NameDb = None
-baseNames = None
-start_optimize = c.Forward()
-end_optimize = c.Forward()
-odd, even0, even1 = [[c.Forward() for p in range(8)] for _ in range(3)]
+    def _optimize(self):
+        if self.optimize_start.IsSet():
+            return
 
+        c.PushTriggerScope()
+        prevTxtPtr, _end = c.Forward(), c.Forward()
+        self.optimize_start << c.NextTrigger()
 
-def _InitPName():
-    global isTxtPtrUnchanged, temp_Db, ptr, epd, temp_Db, odds_or_even
-    global baselens, baseNames
-    isTxtPtrUnchanged = c.EUDLightBool()
-    temp_Db = c.Db(218)
-    ptr, epd = c.EUDVariable(), c.EUDVariable()
-    odds_or_even = c.EUDLightBool()
-    baselens = PVariable()
-    NameDb = [c.Db(26) for _ in range(8)]
-    baseNames = PVariable(NameDb)
+        oddA = EUDArray([ut.EPD(x) for x in self.odd])
+        even0A = EUDArray([ut.EPD(x) for x in self.even0])
+        even1A = EUDArray([ut.EPD(x) for x in self.even1])
 
-
-@c.EUDFunc
-def f_check_id(player, dst):
-    ret = c.EUDVariable()
-    ret << 1
-
-    cs.EUDSwitch(player)
-    for i in range(8):
-        cs.EUDSwitchCase()(i)
-        if cs.EUDIf()(
-            [odds_or_even.IsCleared(), odd[i] << c.MemoryEPD(dst, c.Exactly, 0)]
-        ):
-            ret << 0
-
-        if cs.EUDElseIf()(
-            [
-                odds_or_even,
-                even0[i] << c.MemoryXEPD(dst, c.Exactly, 0, 0xFFFF0000),
-                even1[i] << c.MemoryEPD(dst + 1, c.Exactly, 0),
-            ]
-        ):
-            ret << 0
-        cs.EUDEndIf()
-        cs.EUDBreak()
-    cs.EUDEndSwitch()
-
-    return ret
-
-
-def _OptimizeSetPName():
-    c.PushTriggerScope()
-    global start_optimize, end_optimize, isTxtPtrUnchanged
-    prevTxtPtr, _end = c.Forward(), c.Forward()
-    start_optimize << c.NextTrigger()
-
-    oddA = EUDArray([ut.EPD(x) for x in odd])
-    even0A = EUDArray([ut.EPD(x) for x in even0])
-    even1A = EUDArray([ut.EPD(x) for x in even1])
-
-    if cs.EUDExecuteOnce()():
-        for player in EUDLoopPlayer(None):
-            player_name = 36 * player
-            playerid_epd = 9 * player
-            cs.DoActions(
-                player_name.AddNumber(0x57EEEB),
-                playerid_epd.AddNumber(ut.EPD(0x57EEEC)),
-            )
-            idlen = f_strlen_epd(playerid_epd)
-            nameDb = baseNames[player]
-            f_dbstr_print(nameDb, ptr2s(player_name), ":")
-            val_odd = f_dwread_epd(ut.EPD(nameDb))
-            v0, v1 = f_dwbreak(val_odd)[0:2]
-            val_even0 = v0 * 0x10000
-            val_even1 = v1 + f_wread_epd(ut.EPD(nameDb) + 1, 0) * 0x10000
-            con_odd, con_even = oddA[player], even1A[player]
-            cs.DoActions(
-                idlen.AddNumber(1),  # fix ":" not recognized
-                player_name.AddNumber(1),
-                c.SetMemoryEPD(con_odd + 2, c.SetTo, val_odd),
-                c.SetMemoryEPD(even0A[player] + 2, c.SetTo, val_even0),
-                c.SetMemoryEPD(con_even + 2, c.SetTo, val_even1),
-            )
-            f_dbstr_print(nameDb, ptr2s(player_name), ":")
-            baselens[player] = idlen
-            if cs.EUDIf()(idlen <= 4):
+        if cs.EUDExecuteOnce()():
+            for player in EUDLoopPlayer(None):
+                player_name = 36 * player
+                playerid_epd = 9 * player
                 cs.DoActions(
-                    c.SetMemoryEPD(con_even + 2, c.SetTo, 0),
-                    c.SetMemoryEPD(con_even + 3, c.SetTo, 0x0F000000),
+                    player_name.AddNumber(0x57EEEB),
+                    playerid_epd.AddNumber(ut.EPD(0x57EEEC)),
                 )
-                if cs.EUDIf()(idlen <= 2):
+                idlen = f_strlen_epd(playerid_epd)
+                nameDb = self.basenames[player]
+                f_dbstr_print(nameDb, ptr2s(player_name), ":")
+                val_odd = f_dwread_epd(ut.EPD(nameDb))
+                v0, v1 = f_dwbreak(val_odd)[0:2]
+                val_even0 = v0 * 0x10000
+                val_even1 = v1 + f_wread_epd(ut.EPD(nameDb) + 1, 0) * 0x10000
+                con_odd, con_even = oddA[player], even1A[player]
+                cs.DoActions(
+                    idlen.AddNumber(1),  # fix ":" not recognized
+                    player_name.AddNumber(1),
+                    c.SetMemoryEPD(con_odd + 2, c.SetTo, val_odd),
+                    c.SetMemoryEPD(even0A[player] + 2, c.SetTo, val_even0),
+                    c.SetMemoryEPD(con_even + 2, c.SetTo, val_even1),
+                )
+                f_dbstr_print(nameDb, ptr2s(player_name), ":")
+                self.baselens[player] = idlen
+                if cs.EUDIf()(idlen <= 4):
                     cs.DoActions(
-                        c.SetMemoryEPD(con_odd + 2, c.SetTo, 0),
-                        c.SetMemoryEPD(con_odd + 3, c.SetTo, 0x0F000000),
+                        c.SetMemoryEPD(con_even + 2, c.SetTo, 0),
+                        c.SetMemoryEPD(con_even + 3, c.SetTo, 0x0F000000),
                     )
+                    if cs.EUDIf()(idlen <= 2):
+                        cs.DoActions(
+                            c.SetMemoryEPD(con_odd + 2, c.SetTo, 0),
+                            c.SetMemoryEPD(con_odd + 3, c.SetTo, 0x0F000000),
+                        )
+                    cs.EUDEndIf()
                 cs.EUDEndIf()
-            cs.EUDEndIf()
-    cs.EUDEndExecuteOnce()
+        cs.EUDEndExecuteOnce()
 
-    once = c.Forward()
-    cs.EUDJumpIf([once << c.Memory(0x57F23C, c.Exactly, ~0)], end_optimize)
-    cs.DoActions(
-        isTxtPtrUnchanged.Clear(), c.SetMemory(once + 8, c.SetTo, f_getgametick()),
-    )
-    c.RawTrigger(
-        conditions=[prevTxtPtr << c.Memory(0x640B58, c.Exactly, 11)],
-        actions=isTxtPtrUnchanged.Set(),
-    )
-    cs.EUDJumpIf(isTxtPtrUnchanged, _end)
-    cs.DoActions(c.SetMemory(prevTxtPtr + 8, c.SetTo, 0))
-    for i in range(3, -1, -1):
-        c.RawTrigger(
-            conditions=c.MemoryX(0x640B58, c.AtLeast, 1, 2 ** i),
-            actions=c.SetMemory(prevTxtPtr + 8, c.Add, 2 ** i),
+        once = c.Forward()
+        cs.EUDJumpIf([once << c.Memory(0x57F23C, c.Exactly, ~0)], self.optimize_end)
+        cs.DoActions(
+            self.is_chatptr_unchanged.Clear(),
+            c.SetMemory(once + 8, c.SetTo, f_getgametick()),
         )
-    _end << c.NextTrigger()
-    end_optimize << c.RawTrigger()
-    c.PopTriggerScope()
+        c.RawTrigger(
+            conditions=[prevTxtPtr << c.Memory(0x640B58, c.Exactly, 11)],
+            actions=self.is_chatptr_unchanged.Set(),
+        )
+        cs.EUDJumpIf(self.is_chatptr_unchanged, _end)
+        cs.DoActions(c.SetMemory(prevTxtPtr + 8, c.SetTo, 0))
+        for i in range(3, -1, -1):
+            c.RawTrigger(
+                conditions=c.MemoryX(0x640B58, c.AtLeast, 1, 2 ** i),
+                actions=c.SetMemory(prevTxtPtr + 8, c.Add, 2 ** i),
+            )
+        _end << c.NextTrigger()
+        self.optimize_end << c.RawTrigger()
+        c.PopTriggerScope()
+
+    def check_id(self, player, dst):
+        ret = c.EUDVariable()
+        ret << 1
+
+        cs.EUDSwitch(player)
+        for i in range(8):
+            cs.EUDSwitchCase()(i)
+            if cs.EUDIf()(
+                [
+                    self.odds_or_even.IsCleared(),
+                    self.odd[i] << c.MemoryEPD(dst, c.Exactly, 0),
+                ]
+            ):
+                ret << 0
+
+            if cs.EUDElseIf()(
+                [
+                    self.odds_or_even,
+                    self.even0[i] << c.MemoryXEPD(dst, c.Exactly, 0, 0xFFFF0000),
+                    self.even1[i] << c.MemoryEPD(dst + 1, c.Exactly, 0),
+                ]
+            ):
+                ret << 0
+            cs.EUDEndIf()
+            cs.EUDBreak()
+        cs.EUDEndSwitch()
+
+        return ret
+
+    def set_pname(self, player, *name):
+        self._optimize()
+
+        start = c.Forward()
+        c.RawTrigger(
+            nextptr=self.optimize_start, actions=c.SetNextPtr(self.optimize_end, start)
+        )
+        start << c.NextTrigger()
+
+        _end = c.Forward()
+        cs.EUDJumpIf(self.is_chatptr_unchanged, _end)
+        if ut.isUnproxyInstance(player, type(c.P1)):
+            if player == c.CurrentPlayer:
+                player = f_getcurpl()
+            else:
+                player = c.EncodePlayer(player)
+        cs.EUDJumpIf(f_playerexist(player) == 0, _end)
+        basename, baselen = self.basenames[player], self.baselens[player]
+
+        origcp = f_getcurpl()
+        cs.DoActions(
+            self.ptr.SetNumber(0x640B61),
+            self.epd.SetNumber(ut.EPD(0x640B60)),
+            self.odds_or_even.Clear(),
+        )
+        if cs.EUDWhile()(self.ptr.AtMost(0x640B61 + 218 * 10)):
+            cs.EUDContinueIf(f_check_id(player, self.epd))
+            cs.EUDContinueIf(f_memcmp(basename, self.ptr, baselen) >= 1)
+            cs.DoActions(
+                c.SetCurrentPlayer(ut.EPD(self.db)), baselen.SubtractNumber(1)
+            )  # fix setpname save 1byte less chat contents
+            f_cpstr_print(ptr2s(self.ptr + baselen), EOS=False)
+            cs.DoActions(
+                c.SetDeaths(c.CurrentPlayer, c.SetTo, 0, 0),
+                baselen.AddNumber(1),
+                c.SetCurrentPlayer(self.epd),
+            )
+            c.RawTrigger(
+                conditions=self.odds_or_even.IsSet(),
+                actions=[
+                    c.SetDeaths(c.CurrentPlayer, c.SetTo, ut.b2i4(b"\r" * 4), 0),
+                    c.AddCurrentPlayer(1),
+                ],
+            )
+            name = list(name)
+            name.append(epd2s(ut.EPD(self.db)))
+            f_cpstr_print(*name)
+
+            cs.EUDSetContinuePoint()
+            cs.DoActions(
+                self.ptr.AddNumber(218),
+                self.epd.AddNumber(54),
+                self.odds_or_even.Toggle(),
+            )
+            c.RawTrigger(
+                conditions=self.odds_or_even.IsCleared(), actions=self.epd.AddNumber(1)
+            )
+        cs.EUDEndWhile()
+        f_setcurpl(origcp)
+
+        _end << c.NextTrigger()
+
+
+_global_pname_class = _PlayerName()
+
+
+@c.EUDTypedFunc([c.TrgPlayer, None])
+def f_check_id(player, dst):
+    return _global_pname_class.check_id(player, dst)
+
+
+def SetPName(player, *name):
+    _global_pname_class.set_pname(player, *name)
 
 
 def SetPNamef(player, format_string, *args):
     fmtargs = _format_args(format_string, *args)
     SetPName(player, *fmtargs)
-
-
-def SetPName(player, *name):
-    global isPNameInitialized
-    if not isPNameInitialized:
-        _InitPName()
-        _OptimizeSetPName()
-        isPNameInitialized = True
-
-    start = c.Forward()
-    c.RawTrigger(nextptr=start_optimize, actions=c.SetNextPtr(end_optimize, start))
-    start << c.NextTrigger()
-
-    _end = c.Forward()
-    cs.EUDJumpIf(isTxtPtrUnchanged, _end)
-    if ut.isUnproxyInstance(player, type(c.P1)):
-        if player == c.CurrentPlayer:
-            player = f_getcurpl()
-        else:
-            player = c.EncodePlayer(player)
-    cs.EUDJumpIf(f_playerexist(player) == 0, _end)
-    basename, baselen = baseNames[player], baselens[player]
-
-    global ptr, epd, odds_or_even
-    origcp = f_getcurpl()
-    cs.DoActions(
-        ptr.SetNumber(0x640B61), epd.SetNumber(ut.EPD(0x640B60)), odds_or_even.Clear(),
-    )
-    if cs.EUDWhile()(ptr.AtMost(0x640B61 + 218 * 10)):
-        cs.EUDContinueIf(f_check_id(player, epd))
-        cs.EUDContinueIf(f_memcmp(basename, ptr, baselen) >= 1)
-        cs.DoActions(
-            c.SetCurrentPlayer(ut.EPD(temp_Db)), baselen.SubtractNumber(1)
-        )  # fix setpname save 1byte less chat contents
-        f_cpstr_print(ptr2s(ptr + baselen), EOS=False)
-        cs.DoActions(
-            c.SetDeaths(c.CurrentPlayer, c.SetTo, 0, 0),
-            baselen.AddNumber(1),
-            c.SetCurrentPlayer(epd),
-        )
-        c.RawTrigger(
-            conditions=odds_or_even.IsSet(),
-            actions=[
-                c.SetDeaths(c.CurrentPlayer, c.SetTo, ut.b2i4(b"\r" * 4), 0),
-                c.AddCurrentPlayer(1),
-            ],
-        )
-        name = list(name)
-        name.append(epd2s(ut.EPD(temp_Db)))
-        f_cpstr_print(*name)
-
-        cs.EUDSetContinuePoint()
-        cs.DoActions(ptr.AddNumber(218), epd.AddNumber(54), odds_or_even.Toggle())
-        c.RawTrigger(conditions=odds_or_even.IsCleared(), actions=epd.AddNumber(1))
-    cs.EUDEndWhile()
-    f_setcurpl(origcp)
-
-    _end << c.NextTrigger()
