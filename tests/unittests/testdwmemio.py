bf48d7c130f1a40ad325da814c75ae208b108c28
armoha <kein0011@naver.com>
2021-12-23 19:34:31 +0900

    Add test for posread & cunitread

--- tests/unittests/testdwmemio.py
+++ tests/unittests/testdwmemio.py
@@ -1,4 +1,5 @@
 from helper import *
+import random
 
 
 @TestInstance
@@ -11,6 +12,33 @@ def test_dwmemio():
     # For any ptr, there are 4 valid epd values. So comparing epd and epd
     # for equality won't work. Here we convert epd back to ptr for comparison.
     test_equality("f_dwread_epd", [ptr, 0x58A364 + 4 * epd], [0x1234, 0x1234])
+
+    # Map position reading
+    b1 = random.randint(0, 0xFFFFFFFF)
+    dim = GetChkTokenized().getsection("DIM")
+    bx, by = b2i2(dim), b2i2(dim, 2)
+    bx = 2 ** ((bx - 1).bit_length() + 5) - 1
+    by = 2 ** ((by - 1).bit_length() + 5) - 1
+    bx, by = b1 & bx, (b1 & (by << 16)) >> 16
+
+    x, y = f_posread_epd(EPD(Db(i2b4(b1))))
+    test_equality("f_posread_epd %X" % b1, [x, y], [bx, by])
+
+    # CUnit reading
+    c1 = random.randint(0, 0xFFFFFFFF)
+    if c1 == 0:
+        cptr, cepd = 0, 0
+    else:
+        cptr, cepd = 0x400008, 0xFFF9D729
+        for bit in range(4, 22):
+            bit = 1 << bit
+            if c1 & bit:
+                cptr += bit
+                cepd += bit >> 2
+        cepd = cepd & 0xFFFFFFFF
+
+    ptr, epd = f_cunitepdread_epd(EPD(Db(i2b4(c1))))
+    test_equality("f_cunitread_epd %X" % c1, [ptr, epd], [cptr, cepd])
 
     # Flag reading!
     f1, f2, f3, f4 = f_flagread_epd(EPD(a), 0x1000, 0x0100, ~0x0010, 0x0004)
