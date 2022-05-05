7e6506f6a443520692722233747f8aaa6bcfc89b
armoha <kein0011@naver.com>
2022-05-05 22:59:42 +0900

    Update __init__.py

--- eudplib/eudlib/stringf/__init__.py
+++ eudplib/eudlib/stringf/__init__.py
@@ -36,8 +36,15 @@ from .strbuffer import (
     f_simpleprint,
     f_println,
     f_printAt,
+)
+
+from .strall import (
     f_printAll,
+    DisplayTextAllAt,
     f_printAllAt,
+    f_playWavAll,
+    f_setMissionObjectivesAll,
+
 )
 
 from .strfunc import f_strcpy, f_strcmp, f_strlen, f_strlen_epd, f_strnstr
