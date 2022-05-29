7c74da52eceb682b0ba369795bafba2955d47130
armoha <kein0011@naver.com>
2022-05-30 03:51:17 +0900

    EUDBag: API overhaul

--- eudplib/eudlib/eudbag.py
+++ eudplib/eudlib/eudbag.py
@@ -12,40 +12,59 @@
 """
 
 from .. import core as c
-from ..utils import cachedfunc, ep_assert, ExprProxy
+from ..utils import ep_assert
 
 
-@cachedfunc
-@cachedfunc
-def EUDBag(size, basetype=None):
-    ep_assert(isinstance(size, int) and 1 <= size <= 64)
-
-    class _EUDBag(ExprProxy):
-        def __init__(self, initvars=None, *, capacity, _from=None):
-            # Initialization from value
-            if _from is not None:
-                if IsConstExpr(_from):
-                    baseobj = _from
-
-                # Initialization by variable reference
+class EUDBag:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.loopvar = {}
+        self.cpvar = None
+        self.non_default = []
+        self.default = []
+        self.keyword_only = []
+        args = vars(self.__class__)
+        self.loopvar = {name: EUDVariable() for name in args if not name.startswith("__")}
+        for name, initval in args:
+            # ignore python system attributes
+            if name.startswith("__"):
+                continue
+            # CP trick member
+            if initval is c.CurrentPlayer:
+                ep_assert(self.cpvar is None, f"Multiple CurrentPlayer members are disallowed: {self.cpvar} and {name}")
+                self.cpvar = name
+                self.loopvar[name]._vartrigger._initval = (0xFFFFFFFF, c.EncodePlayer(c.CurrentPlayer), 0, 0x072D0000, 0)
+            # positional args
+            if initval in (None, c.CurrentPlayer):
+                if self.default:
+                    self.keyword_only.append((name, None))
                 else:
-                    baseobj = EUDVariable()
-                    baseobj << _from
-
+                    self.non_default.append(name)
+            elif self.keyword_only:
+                self.keyword_only.append((name, initval))
             else:
-                # Int -> interpret as sequence of 0s
-                if initvars is None:
-                    initvars = [(0,) * size] * size
+                self.default.append((name, initval))
 
-                # For python iterables
-                baseobj = EUDVArrayData(size)(initvars, dest=dest, nextptr=nextptr)
+    def add(self, *args, **kwargs):
+        if len(args) > len(self.non_default) + len(self.default):
+            raise TypeError(f"{self.__class__.__name__}.add takes from {len(self.non_default)} to {len(self.non_default) + len(self.default)} positional arguments but {len(args)} were given")
+        if len(args) < len(self.non_default):
+            missing_args = []
+            for arg in self.non_default[len(args):]:
+                if arg not in kwargs:
+                    missing_args.append(arg)
+            if missing_args:
+                raise TypeError(f"{self.__class__.__name__}.add missing {len(missing_args)} required positional arguments: {*missing_args}")
+        for arg in self.non_default[:len(args)]:
+            if arg in kwargs:
+                raise TypeError(f"{self.__class__.__name__}.add got multiple values for argument {arg}")
+        if self.keyword_only:
+            missing_kwargs = []
+            for kwarg in self.keyword_only:
+                if kwarg not in kwargs:
+                missing_kwargs.append(kwarg)
+            if missing_kwargs:
+                raise TypeError(f"{self.__class__.__name__}.add missing {len(missing_kwargs)} required positional arguments: {*missing_kwargs}")
 
-            super().__init__(baseobj)
-            self.dontFlatten = True
-            self._epd = EPD(self)
-            self._basetype = basetype
-
-        def __iter__(self):
-            pass
-
-    return _EUDBag
+    def __iter__(self):
+        pass
