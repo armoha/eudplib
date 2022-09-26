4ede164d616f7b4939e5675581a05140d2a2914d
armoha <kein0011@naver.com>
2022-09-27 00:52:03 +0900

    Add ItemProxy

--- eudplib/utils/exprproxy.py
+++ eudplib/utils/exprproxy.py
@@ -148,7 +148,10 @@ class ExprProxy:
         return getattr(self._value, name)
 
     def __getitem__(self, name):
-        return self._value[name]
+        return ItemProxy(self._value, name)
+
+    def __iter__(self):
+        return self._value
 
     def __setitem__(self, name, newvalue):
         self._value[name] = newvalue
@@ -168,3 +171,170 @@ def isUnproxyInstance(x, cls):
         return isUnproxyInstance(x.getValue(), cls)
     except (AttributeError, TypeError):
         return False
+
+
+class ItemProxy:
+    def __init__(self, parent, key):
+        self._parent = parent
+        self._key = key
+
+    @classmethod
+    def cast(cls, _from):
+        try:
+            return cls(_from=_from)
+        except TypeError as e:
+            raise TypeError(_("Type {} is not castable").format(cls.__name__), e)
+
+    def getValue(self):
+        return self._parent[self._key]
+
+    def __hash__(self):
+        return id(self)
+
+    # in-place item operations
+    def __iadd__(self, val):
+        if hasattr(self._parent, "iadditem"):
+            self._parent.iadditem(self._key, val)
+        else:
+            ov = self._parent[self._key]
+            ov += v
+            self._parent[self._key] = ov
+        return self
+
+    def __isub__(self, val):
+        if hasattr(self._parent, "isubitem"):
+            self._parent.isubitem(self._key, val)
+        else:
+            ov = self._parent[self._key]
+            ov -= v
+            self._parent[self._key] = ov
+        return self
+
+    def __imul__(self, val):
+        if hasattr(self._parent, "imulitem"):
+            self._parent.imulitem(self._key, val)
+        else:
+            ov = self._parent[self._key]
+            ov *= v
+            self._parent[self._key] = ov
+        return self
+
+    def __ifloordiv__(self, val):
+        if hasattr(self._parent, "ifloordivitem"):
+            self._parent.ifloordivitem(self._key, val)
+        else:
+            ov = self._parent[self._key]
+            ov //= v
+            self._parent[self._key] = ov
+        return self
+
+    def __iand__(self, val):
+        if hasattr(self._parent, "ianditem"):
+            self._parent.ianditem(self._key, val)
+        else:
+            ov = self._parent[self._key]
+            ov &= v
+            self._parent[self._key] = ov
+        return self
+
+    def __ior__(self, val):
+        if hasattr(self._parent, "ioritem"):
+            self._parent.ioritem(self._key, val)
+        else:
+            ov = self._parent[self._key]
+            ov |= v
+            self._parent[self._key] = ov
+        return self
+
+    def __ixor__(self, val):
+        if hasattr(self._parent, "ixoritem"):
+            self._parent.ixoritem(self._key, val)
+        else:
+            ov = self._parent[self._key]
+            ov ^= v
+            self._parent[self._key] = ov
+        return self
+
+    # Proxy arithmetic operators
+    def __lshift__(self, k):
+        return self._parent[self._key] << k
+
+    def __rlshift__(self, k):
+        return k << self._parent[self._key]
+
+    def __rshift__(self, k):
+        return self._parent[self._key] >> k
+
+    def __rrshift__(self, k):
+        return k >> self._parent[self._key]
+
+    def __add__(self, k):
+        return self._parent[self._key] + k
+
+    def __radd__(self, k):
+        return k + self._parent[self._key]
+
+    def __sub__(self, k):
+        return self._parent[self._key] - k
+
+    def __rsub__(self, k):
+        return k - self._parent[self._key]
+
+    def __mul__(self, k):
+        return self._parent[self._key] * k
+
+    def __rmul__(self, k):
+        return k * self._parent[self._key]
+
+    def __floordiv__(self, k):
+        return self._parent[self._key] // k
+
+    def __rfloordiv__(self, k):
+        return k // self._parent[self._key]
+
+    def __and__(self, k):
+        return self._parent[self._key] & k
+
+    def __rand__(self, k):
+        return k & self._parent[self._key]
+
+    def __or__(self, k):
+        return self._parent[self._key] | k
+
+    def __ror__(self, k):
+        return k | self._parent[self._key]
+
+    def __xor__(self, k):
+        return self._parent[self._key] ^ k
+
+    def __rxor__(self, k):
+        return k ^ self._parent[self._key]
+
+    def __neg__(self):
+        return -self._parent[self._key]
+
+    def __invert__(self):
+        return ~self._parent[self._key]
+
+    # Proxy comparison operator
+
+    def __eq__(self, k):
+        return self._parent[self._key] == k
+
+    def __ne__(self, k):
+        return self._parent[self._key] != k
+
+    def __le__(self, k):
+        return self._parent[self._key] <= k
+
+    def __lt__(self, k):
+        return self._parent[self._key] < k
+
+    def __ge__(self, k):
+        return self._parent[self._key] >= k
+
+    def __gt__(self, k):
+        return self._parent[self._key] > k
+
+    def __bool__(self):
+        return bool(self._parent[self._key])
