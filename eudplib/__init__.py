e03cd051bdd5dfbb71d6d64095c7d8714648ed27
armoha <kein0011@naver.com>
2022-10-09 12:52:27 +0900

    eudplib 0.70.2

--- eudplib/__init__.py
+++ eudplib/__init__.py
@@ -45,7 +45,7 @@ from .trigtrg.runtrigtrg import (
 from .maprw import *
 
 
-__version__ = "0.70.1"
+__version__ = "0.70.2"
 
 import types
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
         return None
 
 
-__version__ = "0.70.1"
+__version__ = "0.70.2"
 
 
 setup(
