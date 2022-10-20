55e09537a4cb2f95b073302ce770a8f2c4556125
armoha <kein0011@naver.com>
2022-10-21 02:56:52 +0900

    EUDDeque: implement __iter__ Fixes EUDQueue.__iter__ corrupted trigger flow

--- eudplib/eudlib/eudqueue.py
+++ eudplib/eudlib/eudqueue.py
@@ -70,6 +70,7 @@ def EUDQueue(capacity):
                     c.SetNextPtr(pop.GetVTable(), 0),  # yield_point by __iter__ call
                     SetMemory(iter_start + 16, Add, 18),
                     SetMemory(iter_jump + 4, Add, 72),
+                    c.SetMemoryX(iter_start + 2376, c.SetTo, 0, 1),
                 ],
             )
             iter_contpoint = c.RawTrigger(
@@ -99,11 +100,7 @@ def EUDQueue(capacity):
             @_EUDPredefineParam((EPD(append_act) + 5,))
             @c.EUDFunc
             def append(value):
-                check_wraparound, wraparound_tail, wraparound_head = (
-                    c.Forward(),
-                    c.Forward(),
-                    c.Forward(),
-                )
+                check_wrap, wrap_tail, wrap_head = c.Forward(), c.Forward(), c.Forward()
                 c.RawTrigger(
                     actions=[
                         append_act,
@@ -111,27 +108,27 @@ def EUDQueue(capacity):
                         self._length.AddNumber(1),
                     ],
                 )
-                check_wraparound << c.RawTrigger(
-                    nextptr=wraparound_head,
+                check_wrap << c.RawTrigger(
+                    nextptr=wrap_head,
                     conditions=self._length.AtLeast(capacity + 1),
                     actions=[
-                        c.SetNextPtr(check_wraparound, wraparound_tail),
+                        c.SetNextPtr(check_wrap, wrap_tail),
                         SetMemory(jump + 4, Add, 72),
                         SetMemory(iter_init + 348, Add, 18),
                         SetMemory(iter_init + 380, Add, 72),
                         self._length.SubtractNumber(1),
                     ],
                 )
-                wraparound_tail << c.RawTrigger(
+                wrap_tail << c.RawTrigger(
                     conditions=Memory(jump + 4, c.AtLeast, queue + 72 * (capacity - 1)),
                     actions=[
-                        c.SetNextPtr(check_wraparound, wraparound_head),
+                        c.SetNextPtr(check_wrap, wrap_head),
                         SetMemory(jump + 4, Add, -(72 * capacity)),
                         SetMemory(iter_init + 348, Add, -(18 * capacity)),
                         SetMemory(iter_init + 380, Add, -(72 * capacity)),
                     ],
                 )
-                wraparound_head << c.RawTrigger(
+                wrap_head << c.RawTrigger(
                     conditions=Memory(append_act + 16, c.AtLeast, EPD(queue) + 87 + 18 * capacity),
                     actions=SetMemory(append_act + 16, Add, -(18 * capacity)),
                 )
@@ -172,14 +169,14 @@ def EUDQueue(capacity):
             popleft()  # by always instantiate triggers
             c.PopTriggerScope()
 
+        def __iter__(self):
+            return self._iter()
+
         def append(self, value, **kwargs):
             self._append(value, **kwargs)
 
         def popleft(self, **kwargs):
             return self._popleft(**kwargs)
-
-        def __iter__(self):
-            return self._iter()
 
         def empty(self):
             return self._length == 0
@@ -236,28 +233,100 @@ def EUDDeque(capacity):
             jump, jumpleft = c.Forward(), c.Forward()
             self._length = c.EUDVariable(0)
 
+            c.PushTriggerScope()
+            iter_start, iter_jump = c.Forward(), c.Forward()
+            iter_init = c.RawTrigger(
+                actions=[
+                    SetMemory(iter_start + 16, c.SetTo, EPD(deque) + 87),
+                    c.SetNextPtr(iter_jump, deque - 72),
+                    c.SetNextPtr(iter_start, iter_jump),
+                    c.SetMemoryX(iter_start + 2376, c.SetTo, 1, 1),
+                ],
+            )
+            c.RawTrigger(
+                conditions=self._length.AtLeast(1),
+                actions=c.SetMemoryX(iter_start + 2404, c.SetTo, 1 << 24, 255 << 24),
+            )
+            iter_start << c.RawTrigger(
+                nextptr=0,  # by iter_init
+                conditions=Memory(append_act + 16, c.Exactly, 0),  # by iter_init, iter_contpoint
+                actions=c.SetNextPtr(iter_start, 0),  # end by __iter__ call
+            )
+            iter_jump << c.RawTrigger(
+                nextptr=0,  # by iter_init, iter_contpoint
+                actions=[
+                    c.SetNextPtr(pop.GetVTable(), 0),  # yield_point by __iter__ call
+                    SetMemory(iter_start + 16, Add, 18),
+                    SetMemory(iter_jump + 4, Add, 72),
+                    c.SetMemoryX(iter_start + 2376, c.SetTo, 0, 1),
+                ],
+            )
+            iter_contpoint = c.RawTrigger(
+                nextptr=iter_start,
+                conditions=Memory(iter_start + 16, c.AtLeast, EPD(deque) + 87 + 18 * capacity),
+                actions=[
+                    SetMemory(iter_start + 16, Add, -(18 * capacity)),
+                    SetMemory(iter_jump + 4, Add, -(72 * capacity)),
+                ],
+            )
+            c.PopTriggerScope()
+
+            def iter():
+                yield_point, end = c.Forward(), c.Forward()
+                c.RawTrigger(
+                    nextptr=iter_init,
+                    actions=[
+                        SetMemory(iter_start + 348, c.SetTo, end),
+                        SetMemory(iter_jump + 348, c.SetTo, yield_point),
+                    ],
+                )
+                yield_point << c.NextTrigger()
+                yield ret
+                c.SetNextTrigger(iter_contpoint)
+                end << c.NextTrigger()
+
             @_EUDPredefineParam((EPD(append_act) + 5,))
             @c.EUDFunc
             def append(value):
                 # deque[head] = value
                 # head %+ 1
+                check_wrap, wrap_tail, wrap_head = c.Forward(), c.Forward(), c.Forward()
                 c.RawTrigger(
                     actions=[
                         append_act,
                         SetMemory(append_act + 16, Add, 18),
                         SetMemory(jump + 4, Add, 72),
+                        self._length.AddNumber(1),
                     ],
                 )
-                c.RawTrigger(
+                check_wrap << c.RawTrigger(
+                    nextptr=wrap_head,
+                    conditions=self._length.AtLeast(capacity + 1),
+                    actions=[
+                        c.SetNextPtr(check_wrap, wrap_tail),
+                        SetMemory(appendleft_act + 16, Add, 18),
+                        SetMemory(jumpleft + 4, Add, 72),
+                        SetMemory(iter_init + 348, Add, 18),
+                        SetMemory(iter_init + 380, Add, 72),
+                        self._length.SubtractNumber(1),
+                    ],
+                )
+                wrap_tail << c.RawTrigger(
+                    conditions=Memory(jump + 4, c.AtLeast, deque + 72 * (capacity - 1)),
+                    actions=[
+                        c.SetNextPtr(check_wrap, wrap_head),
+                        SetMemory(appendleft_act + 16, Add, -(18 * capacity)),
+                        SetMemory(jumpleft + 4, Add, -(72 * capacity)),
+                        SetMemory(iter_init + 348, Add, -(18 * capacity)),
+                        SetMemory(iter_init + 380, Add, -(72 * capacity)),
+                    ],
+                )
+                wrap_head << c.RawTrigger(
                     conditions=Memory(append_act + 16, c.AtLeast, EPD(deque) + 87 + 18 * capacity),
                     actions=[
                         SetMemory(append_act + 16, Add, -(18 * capacity)),
                         SetMemory(jump + 4, Add, -(72 * capacity)),
                     ],
-                )
-                c.RawTrigger(
-                    conditions=self._length.AtMost(capacity - 1),
-                    actions=self._length.AddNumber(1),
                 )
 
             @c.EUDFunc
@@ -290,11 +359,32 @@ def EUDDeque(capacity):
             def appendleft(value):
                 # tail %- 1
                 # deque[tail] = value
-                c.RawTrigger(
+                check_wrap, wrap_head, wrap_tail = c.Forward(), c.Forward(), c.Forward()
+                check_wrap << c.RawTrigger(
+                    nextptr=wrap_tail,
+                    conditions=self._length.AtLeast(capacity),
+                    actions=[
+                        c.SetNextPtr(check_wrap, wrap_head),
+                        SetMemory(append_act + 16, Add, -18),
+                        SetMemory(jump + 4, Add, -72),
+                        self._length.SubtractNumber(1),
+                    ],
+                )
+                wrap_head << c.RawTrigger(
+                    conditions=Memory(jump + 4, c.AtMost, deque),
+                    actions=[
+                        c.SetNextPtr(check_wrap, wrap_tail),
+                        SetMemory(append_act + 16, Add, 18 * capacity),
+                        SetMemory(jump + 4, Add, 72 * capacity),
+                    ],
+                )
+                wrap_tail << c.RawTrigger(
                     conditions=Memory(appendleft_act + 16, c.AtMost, EPD(deque) + 87),
                     actions=[
                         SetMemory(appendleft_act + 16, Add, 18 * capacity),
                         SetMemory(jumpleft + 4, Add, 72 * capacity),
+                        SetMemory(iter_init + 348, Add, 18 * capacity),
+                        SetMemory(iter_init + 380, Add, 72 * capacity),
                     ],
                 )
                 c.RawTrigger(
@@ -302,11 +392,8 @@ def EUDDeque(capacity):
                         SetMemory(appendleft_act + 16, Add, -18),
                         SetMemory(jumpleft + 4, Add, -72),
                         appendleft_act,
+                        self._length.AddNumber(1),
                     ],
-                )
-                c.RawTrigger(
-                    conditions=self._length.AtMost(capacity - 1),
-                    actions=self._length.AddNumber(1),
                 )
 
             @c.EUDFunc
@@ -320,9 +407,11 @@ def EUDDeque(capacity):
                     nextptr=deque - 72,
                     actions=[
                         c.SetNextPtr(pop.GetVTable(), wraparound),
-                        self._length.SubtractNumber(1),
                         SetMemory(appendleft_act + 16, Add, 18),
                         SetMemory(jumpleft + 4, Add, 72),
+                        SetMemory(iter_init + 348, Add, 18),
+                        SetMemory(iter_init + 380, Add, 72),
+                        self._length.SubtractNumber(1),
                     ],
                 )
                 wraparound << c.RawTrigger(
@@ -330,6 +419,8 @@ def EUDDeque(capacity):
                     actions=[
                         SetMemory(appendleft_act + 16, Add, -(18 * capacity)),
                         SetMemory(jumpleft + 4, Add, -(72 * capacity)),
+                        SetMemory(iter_init + 348, Add, -(18 * capacity)),
+                        SetMemory(iter_init + 380, Add, -(72 * capacity)),
                     ],
                 )
                 return ret
@@ -338,6 +429,7 @@ def EUDDeque(capacity):
             self._appendleft = appendleft
             self._pop = _pop
             self._popleft = popleft
+            self._iter = iter
 
             c.PushTriggerScope()
             append(0)  # prevent 'Cannot evaluate <ConstExpr>...' error
@@ -345,6 +437,9 @@ def EUDDeque(capacity):
             appendleft(0)
             popleft()
             c.PopTriggerScope()
+
+        def __iter__(self):
+            return self._iter()
 
         def append(self, value, **kwargs):
             self._append(value, **kwargs)
