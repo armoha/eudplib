97b34ec2faf1d3c5ec7f240a471691ce6c63d02a
armoha <kein0011@naver.com>
2023-01-14 22:20:32 +0900

    EUDStructArray: dynamic class to static class

--- eudplib/core/eudstruct/structarr.py
+++ eudplib/core/eudstruct/structarr.py
@@ -29,64 +29,76 @@ from .vararray import EUDVArray
 
 
 class _EUDStruct_Metaclass(type):
-    def __init__(cls, name, bases, dct):
+    def __init__(cls, name, bases, dct) -> None:
         # For field declaration, modify selftype to cls
         try:
             fields = dct["_fields_"]
-            for i in range(len(fields)):
-                member = fields[i]
-
+        except KeyError:
+            pass
+        else:
+            for i, member in enumerate(fields):
                 if isinstance(member, str):
                     continue
                 mName, mType = member
                 if mType is selftype:
                     fields[i] = (mName, cls)
-        except KeyError as e:
-            pass
 
         super().__init__(name, bases, dct)
 
-    def __mul__(self, times):
+    def __mul__(self, times: int):
         basetype = self
+        return _EUDStructArray(times, basetype)
 
-        class EUDStructArray(ExprProxy):
-            __metaclass__ = _EUDStruct_Metaclass
 
-            def __init__(self, initvar=None, *, _from=None):
-                if _from is None:
-                    if initvar is None:
-                        initvals = [0 for _ in range(times)]
-                        super().__init__(EUDVArray(times, basetype)(initvals))
-                    else:
-                        super().__init__(EUDVArray(times, basetype)(initvar))
-                else:
-                    super().__init__(EUDVArray(times, basetype).cast(_from))
+class EUDStructArray(ExprProxy, metaclass=_EUDStruct_Metaclass):
+    def __init__(self, initvar=None, *, _from=None, _times, _basetype):
+        if _from is None:
+            if initvar is None:
+                initvals = [0] * _times
+                super().__init__(EUDVArray(_times, _basetype)(initvals))
+            else:
+                super().__init__(EUDVArray(_times, _basetype)(initvar))
+        else:
+            super().__init__(EUDVArray(_times, _basetype).cast(_from))
 
-                self._initialized = True
-                self.dontFlatten = True
+        self._initialized = True
+        self.dontFlatten = True
+        self._times = _times
 
-            def copy(self):
-                """Create a shallow copy"""
-                arraytype = type(self)
-                inst = arraytype()
-                self.copyTo(inst)
-                return inst
+    def copy(self):
+        """Create a shallow copy"""
+        arraytype = type(self)
+        inst = arraytype()
+        self.copyTo(inst)
+        return inst
 
-            def copyTo(self, inst):
-                """Copy struct to other instance"""
-                for i in range(times):
-                    inst[i] = self[i]
+    def copyTo(self, inst):
+        """Copy struct to other instance"""
+        for i in range(self._times):
+            inst[i] = self[i]
 
-            def __getitem__(self, index):
-                return self.getValue()[index]
+    def __getitem__(self, index):
+        return self.getValue()[index]
 
-            def __setitem__(self, index, newval):
-                self.getValue()[index] = newval
+    def __setitem__(self, index, newval):
+        self.getValue()[index] = newval
 
-            def __getattr__(self, name):
-                return super().__getattr__(name)
+    def __getattr__(self, name):
+        return super().__getattr__(name)
 
-            def __setattr__(self, name, value):
-                self.__dict__[name] = value
+    def __setattr__(self, name, value):
+        self.__dict__[name] = value
 
-        return EUDStructArray
+
+class _EUDStructArray:
+    def __init__(self, times: int, basetype) -> None:
+        self._times: int = times
+        self._basetype = basetype
+
+    def __call__(self, initvar=None, *, _from=None) -> EUDStructArray:
+        return EUDStructArray(
+            initvar=initvar, _from=_from, _times=self._times, _basetype=self._basetype
+        )
+
+    def cast(self, _from):
+        return self(_from=_from)
