79c230d0abb08b0f3f59445d072c24ed23a7f4d8
armoha <kein0011@naver.com>
2023-01-14 16:39:46 +0900

    Simplify builtin check

--- eudplib/epscript/cpp/parser/parserUtils.cpp
+++ eudplib/epscript/cpp/parser/parserUtils.cpp
@@ -10,7 +10,6 @@
 
 extern int currentTokenizingLine;
 extern std::string currentModule;
-extern std::string pyBuiltinSet;
 
 int errorn = 0;
 
@@ -153,5 +152,5 @@ std::string addStubCode(const std::string& s) {
 
 bool checkPyBuiltinForEpsGlobalConst(std::string& name) {
     if(strncmp(currentModule.c_str(), "BGM", 3) == 0 && name == "str") return false;
-    return true;
+    return isPyBuiltin(name);
 }
