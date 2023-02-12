4717bbb91bff1c5c3fe34f92d99ee66b01dc5ba9
armoha <kein0011@naver.com>
2023-02-12 18:07:18 +0900

    Generate layout with bufgen.rs

---
+++ eudplib/eudbag/bufgen.rs
@@ -0,0 +1,349 @@
+use std::fmt;
+use std::fs::File;
+use std::io::{BufWriter, Write};
+
+#[deny(non_snake_case)]
+#[derive(Clone, Debug)]
+enum Item {
+    Space(u16),
+    NextPtr,
+    NoCondType,
+    NoCondFlag,
+    BitMask(u16),
+    DestValueUnitActTypeModifierActFlagSC(u16),
+    NoActType,
+    NoActFlag,
+    TrgFlag,
+    Breakpoint,
+}
+
+impl Item {
+    fn size(&self) -> u16 {
+        use Item::*;
+        match self {
+            Breakpoint => 0,
+            Space(spaces) => *spaces,
+            NoCondType | NoCondFlag | NoActType | NoActFlag | TrgFlag => 1,
+            NextPtr | BitMask(_) => 4,
+            DestValueUnitActTypeModifierActFlagSC(_) => 16,
+        }
+    }
+    fn is_zero(&self) -> bool {
+        use Item::*;
+        match self {
+            NoCondType | NoCondFlag | NoActType | NoActFlag => true,
+            _ => false,
+        }
+    }
+}
+
+struct Layout {
+    layout: Vec<Item>,
+    actno: u16,
+    distance: u16,
+}
+impl Layout {
+    fn find_insert(&self, index: u16, new_item: Item) -> Option<(usize, Vec<Item>)> {
+        use Item::*;
+        match new_item {
+            Space(_) => panic!("Space can't be inserted"),
+            NextPtr | NoCondType | NoCondFlag => {
+                panic!("Inserted initial items: {:?}", new_item)
+            }
+            _ => (),
+        };
+        let size = new_item.size();
+        let mut pos = 0;
+        for (n, item) in self.layout.iter().enumerate() {
+            if new_item.is_zero() && pos == index && item.is_zero() {
+                return Some((n, vec![new_item]));
+            }
+            if pos <= index && pos + item.size() >= index + size {
+                if size == 0 {
+                    return if pos + item.size() == index {
+                        Some((n, vec![item.clone(), new_item]))
+                    } else if pos == index {
+                        return Some((n, vec![new_item, item.clone()]));
+                    } else {
+                        match item {
+                            Space(spaces) => {
+                                let mut ret = vec![];
+                                if pos < index {
+                                    ret.push(Space(index - pos));
+                                }
+                                ret.push(new_item);
+                                if pos + spaces > index {
+                                    ret.push(Space(spaces - (index - pos)));
+                                }
+                                return Some((n, ret));
+                            }
+                            _ => None,
+                        }
+                    };
+                }
+                match item {
+                    Space(spaces) => {
+                        let mut ret = vec![];
+                        if pos < index {
+                            ret.push(Space(index - pos));
+                        }
+                        ret.push(new_item);
+                        if pos + spaces > index + size {
+                            ret.push(Space(spaces - (index - pos + size)));
+                        }
+                        return Some((n, ret));
+                    }
+                    _ => (),
+                }
+            }
+            pos += item.size();
+        }
+        None
+    }
+    fn insert(&mut self, offset: u16, item: Item) -> Option<()> {
+        println!("insert {item:?} at {offset}");
+        match self.find_insert(offset, item) {
+            Some((n, ret)) => {
+                self.layout.remove(n);
+                println!("... Ok! {ret:?} at {n}");
+                for item in ret.into_iter().rev() {
+                    self.layout.insert(n, item);
+                }
+                Some(())
+            }
+            None => None,
+        }
+    }
+    fn with_action_count_and_distance(actno: u16, distance: u16) -> Option<Layout> {
+        use Item::*;
+        let mut layout = Layout {
+            layout: vec![
+                NextPtr,
+                Space(15),
+                NoCondType,
+                Space(1),
+                NoCondFlag,
+                Space(distance - 22),
+            ],
+            actno: actno,
+            distance: distance,
+        };
+        for k in 0..actno {
+            layout.insert((k * 32 + 324) % distance, BitMask(k))?;
+            layout.insert(
+                (k * 32 + 340) % distance,
+                DestValueUnitActTypeModifierActFlagSC(k),
+            )?;
+        }
+        if 1 <= actno && actno < 64 {
+            layout.insert((actno * 32 + 350) % distance, NoActType)?;
+            layout.insert((actno * 32 + 352) % distance, NoActFlag)?;
+        }
+        layout.insert(2372 % distance, TrgFlag)?;
+        if 2376 % distance != 0 {
+            layout.insert(2376 % distance, Breakpoint)?;
+        }
+        Some(layout)
+    }
+}
+
+impl fmt::Display for Layout {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(
+            f,
+            "def write{}(buf, count, nptr, val):
+    for i in range({} + count):",
+            self.actno,
+            2375 / self.distance
+        )?;
+        for item in self.layout.iter() {
+            use Item::*;
+            match item {
+                Space(size) => writeln!(f, "        buf.WriteSpace({})", size)?,
+                NextPtr => writeln!(
+                    f,
+                    "        buf.WriteDword(nptr[i]) if i < count else buf.WriteSpace(4)"
+                )?,
+                NoCondType => writeln!(
+                    f,
+                    "        buf.WriteByte(0) if i < count else buf.WriteSpace(1)  # nocond"
+                )?,
+                NoCondFlag => writeln!(
+                    f,
+                    "        buf.WriteByte(0) if i < count else buf.WriteSpace(1)  # condflag"
+                )?,
+                BitMask(n) => writeln!(
+                    f,
+                    "        buf.WriteDword(val[i - {1}][{0}][0]) if {1} <= i < {1} + count else buf.WriteSpace(4)",
+                    n,
+                    (324 + 32 * n) / self.distance
+                )?,
+                DestValueUnitActTypeModifierActFlagSC(n) => writeln!(
+                    f,
+                    "        if {1} <= i < {1} + count:
+            buf.WriteDword(val[i - {1}][{0}][1])
+            buf.WriteDword(val[i - {1}][{0}][2])
+            buf.WriteDword(val[i - {1}][{0}][3])
+            buf.WriteDword(val[i - {1}][{0}][4])
+        else:
+            buf.WriteSpace(16)",
+                    n,
+                    (340 + 32 * n) / self.distance
+                )?,
+                NoActType => writeln!(
+                    f,
+                    "        buf.WriteByte(0) if {0} <= i < {0} + count else buf.WriteSpace(1)  # noact",
+                    (self.actno * 32 + 350) / self.distance
+                )?,
+                NoActFlag => writeln!(
+                    f,
+                    "        buf.WriteByte(0) if {0} <= i < {0} + count else buf.WriteSpace(1)  # actflag",
+                    (self.actno * 32 + 352) / self.distance
+                )?,
+                TrgFlag => writeln!(
+                    f,
+                    "        buf.WriteByte(4) if {} <= i else buf.WriteSpace(1)",
+                    2375 / self.distance
+                )?,
+                Breakpoint => writeln!(
+                    f,
+                    "        if i == {} + count - 1:
+            break",
+                    2375 / self.distance
+                )?,
+            }
+        }
+        Ok(())
+    }
+}
+
+fn main() {
+    #[rustfmt::skip]
+    let distance = vec![
+      20,
+      72,  132,  212,  228,  260,  260,  292,  292,
+     616,  648,  680,  708,  740,  772,  812,  844,
+     876,  908,  940,  972, 1004, 1028, 1060, 1092,
+    1124, 1156, 1204, 1236, 1268, 1300, 1332, 1364,
+    1396, 1428, 1460, 1492, 1524, 1556, 1588, 1620,
+    1652, 1684, 1716, 1748, 1780, 1812, 1844, 1876,
+    1908, 1940, 1972, 2004, 2036, 2052, 2084, 2116,
+    2148, 2180, 2212, 2244, 2276, 2308, 2340, 2376];
+
+    let f = File::create("writebuf.py").expect("Unable to create file");
+    let mut f = BufWriter::new(f);
+    writeln!(f, "# Generated by bufgen.rs (DO NOT MODIFY manually!)\n").unwrap();
+    for n in 1..=64 {
+        let layout = Layout::with_action_count_and_distance(n as u16, distance[n] as u16)
+            .expect(&format!("Failed {}: {}", n, distance[n]));
+        write!(f, "{layout}\n\n").unwrap();
+    }
+    match Layout::with_action_count_and_distance(8, 292) {
+        Some(layout) => {
+            println!("{}", layout);
+            for item in &layout.layout {
+                match item {
+                    Space(size) => print!("_{size}"),
+                    NextPtr => print!("N"),
+                    NoCondType => print!("x"),
+                    NoCondFlag => print!("f"),
+                    BitMask(n) => print!("m{n}"),
+                    DestValueUnitActTypeModifierActFlagSC(n) => print!("dvmS{n}"),
+                    NoActType => print!("X"),
+                    NoActFlag => print!("F"),
+                    TrgFlag => print!("T"),
+                    Breakpoint => print!("|"),
+                }
+            }
+            println!("");
+        }
+        None => (),
+    };
+    /*
+    > 8 action size: 292, 197.26027397260273%
+    epd 0 : ['t9nextptr']
+    epd 1~3 are empty
+    epd 4 : ['t9nocond']
+    epd 5 : ['t7noact', 't9condflag=0']
+    epd 6 : ['t7actflag=0']
+    epd 7 is empty
+    epd 8 : ['t8a0mask=~0']
+    epd 9 : ['t1flags']
+    epd 10~11 are empty
+    epd 12 : ['t8a0dest']
+    epd 13 : ['t8a0value']
+    epd 14 : ['t8a0unit=0', 't8a0type=45', 't8a0SetTo']
+    epd 15 : ['t8a0flags', 't8a0SC']
+    epd 16 : ['t8a1mask=~0']
+    epd 17~19 are empty
+    epd 20 : ['t8a1dest']
+    epd 21 : ['t8a1value']
+    epd 22 : ['t8a1unit=0', 't8a1type=45', 't8a1SetTo']
+    epd 23 : ['t8a1flags', 't8a1SC']
+    epd 24 : ['t8a2mask=~0']
+    epd 25~27 are empty
+    epd 28 : ['t8a2dest']
+    epd 29 : ['t8a2value']
+    epd 30 : ['t8a2unit=0', 't8a2type=45', 't8a2SetTo']
+    epd 31 : ['t8a2flags', 't8a2SC']
+    epd 32 : ['t8a3mask=~0']
+    epd 33~35 are empty
+    epd 36 : ['t8a3dest']
+    epd 37 : ['t8a3value']
+    epd 38 : ['t8a3unit=0', 't8a3type=45', 't8a3SetTo']
+    epd 39 : ['t8a3flags', 't8a3SC']
+    epd 40 : ['t8a4mask=~0']
+    epd 41~43 are empty
+    epd 44 : ['t8a4dest']
+    epd 45 : ['t8a4value']
+    epd 46 : ['t8a4unit=0', 't8a4type=45', 't8a4SetTo']
+    epd 47 : ['t8a4flags', 't8a4SC']
+    epd 48 : ['t8a5mask=~0']
+    epd 49~51 are empty
+    epd 52 : ['t8a5dest']
+    epd 53 : ['t8a5value']
+    epd 54 : ['t8a5unit=0', 't8a5type=45', 't8a5SetTo']
+    epd 55 : ['t8a5flags', 't8a5SC']
+    epd 56 : ['t8a6mask=~0']
+    epd 57~59 are empty
+    epd 60 : ['t8a6dest']
+    epd 61 : ['t8a6value']
+    epd 62 : ['t8a6unit=0', 't8a6type=45', 't8a6SetTo']
+    epd 63 : ['t8a6flags', 't8a6SC']
+    epd 64 : ['t8a7mask=~0']
+    epd 65~67 are empty
+    epd 68 : ['t8a7dest']
+    epd 69 : ['t8a7value']
+    epd 70 : ['t8a7unit=0', 't8a7type=45', 't8a7SetTo']
+    epd 71 : ['t8a7flags', 't8a7SC']
+    epd 72 is empty
+    */
+    use Item::*;
+    #[rustfmt::skip]
+    let layout = vec![
+        NextPtr, Space(15), NoCondType, Space(2), NoActType, Space(9),
+        BitMask(0), TrgFlag, Space(3), Breakpoint, Space(8), DestValueUnitActTypeModifierActFlagSC(0),
+        BitMask(1), Space(12),         DestValueUnitActTypeModifierActFlagSC(1),
+        BitMask(2), Space(12),         DestValueUnitActTypeModifierActFlagSC(2),
+        BitMask(3), Space(12),         DestValueUnitActTypeModifierActFlagSC(3),
+        BitMask(4), Space(12),         DestValueUnitActTypeModifierActFlagSC(4),
+        BitMask(5), Space(12),         DestValueUnitActTypeModifierActFlagSC(5),
+        BitMask(6), Space(12),         DestValueUnitActTypeModifierActFlagSC(6),
+        BitMask(7), Space(12),         DestValueUnitActTypeModifierActFlagSC(7),
+        Space(4),
+    ];
+    for item in &layout {
+        match item {
+            Space(size) => print!("_{size}"),
+            NextPtr => print!("N"),
+            NoCondType => print!("x"),
+            NoCondFlag => print!("f"),
+            BitMask(n) => print!("m{n}"),
+            DestValueUnitActTypeModifierActFlagSC(n) => print!("dvmS{n}"),
+            NoActType => print!("X"),
+            NoActFlag => print!("F"),
+            TrgFlag => print!("T"),
+            Breakpoint => print!("|"),
+        }
+    }
+}
