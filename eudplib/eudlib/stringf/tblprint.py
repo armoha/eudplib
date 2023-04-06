e2f148cac4c07b2e2eb768332f5c880d9c64c0c4
armoha <kein0011@naver.com>
2023-04-06 14:58:32 +0900

    Allow to overwrite stat_txt.tbl default encoding

--- eudplib/eudlib/stringf/tblprint.py
+++ eudplib/eudlib/stringf/tblprint.py
@@ -13,30 +13,56 @@ from .eudprint import f_dbstr_print
 from .fmtprint import f_sprintf
 
 
-def f_settbl(tblID, offset, *args, encoding="cp949"):
+class _StatTxtTblEncoding:
+    encoding = "cp949"
+
+
+def f_settbl(tblID, offset, *args, encoding=None):
+    args = ut.FlattenList(args)
     dst = GetTBLAddr(tblID)
     dst += offset
+
+    if encoding is None:
+        encoding = _StatTxtTblEncoding.encoding
+
     if encoding.casefold() == "UTF-8".casefold():
-        f_dbstr_print(dst, *args, "\u2009", encoding="UTF-8")
-    else:
-        f_dbstr_print(dst, *args, encoding=encoding)
+        if isinstance(args[-1], str):
+            if args[-1][-1] != "\u2009":
+                args[-1] = args[-1] + "\u2009"
+        else:
+            args.append("\u2009")
+
+    return f_dbstr_print(dst, *args, encoding=encoding)
 
 
-def f_settbl2(tblID, offset, *args, encoding="cp949"):
+def f_settbl2(tblID, offset, *args, encoding=None):
     dst = GetTBLAddr(tblID)
     dst += offset
-    f_dbstr_print(dst, *args, EOS=False, encoding=encoding)
+
+    if encoding is None:
+        encoding = _StatTxtTblEncoding.encoding
+
+    return f_dbstr_print(dst, *args, EOS=False, encoding=encoding)
 
 
-def f_settblf(tblID, offset, format_string, *args, encoding="cp949"):
+def f_settblf(tblID, offset, format_string, *args, encoding=None):
     dst = GetTBLAddr(tblID)
     dst += offset
+
+    if encoding is None:
+        encoding = _StatTxtTblEncoding.encoding
+
     if encoding.casefold() == "UTF-8".casefold() and format_string[-1] != "\u2009":
         format_string += "\u2009"
-    f_sprintf(dst, format_string, *args, encoding=encoding)
+
+    return f_sprintf(dst, format_string, *args, encoding=encoding)
 
 
-def f_settblf2(tblID, offset, format_string, *args, encoding="cp949"):
+def f_settblf2(tblID, offset, format_string, *args, encoding=None):
     dst = GetTBLAddr(tblID)
     dst += offset
-    f_sprintf(dst, format_string, *args, EOS=False, encoding=encoding)
+
+    if encoding is None:
+        encoding = _StatTxtTblEncoding.encoding
+
+    return f_sprintf(dst, format_string, *args, EOS=False, encoding=encoding)
