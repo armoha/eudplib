e25eb01cc95ec2af55c5ada846dda0f7f5bffe74
armoha <kein0011@naver.com>
2023-06-13 18:21:03 +0900

    eudplib 0.75.0.a17

--- eudplib/eudbag/__init__.py
+++ eudplib/eudbag/__init__.py
@@ -4,4 +4,4 @@
 # All rights reserved.
 # This file is part of EUD python library (eudplib), and is released under "MIT License Agreement".
 # Please see the LICENSE file that should have been included as part of this package.
-from eudbag import EUDBag
+from .eudbag import EUDBag
