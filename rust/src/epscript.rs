493ee47e7e5be08ef7b9e1347890629bad160245
Armoha <kein0011@naver.com>
2024-01-07 00:35:17 +0900

    Rewrite linetable calculating code in Rust

--- rust/src/epscript.rs
+++ rust/src/epscript.rs
@@ -5,8 +5,7 @@ mod linetable;
 
 pub(crate) fn create_submodule(py: Python<'_>) -> PyResult<&PyModule> {
     let submod = PyModule::new(py, "epscript")?;
-    submod.add_function(wrap_pyfunction!(linetable::calculate_linetable, submod)?)?;
-    submod.add_function(wrap_pyfunction!(linetable::print_linetable, submod)?)?;
+    submod.add_function(wrap_pyfunction!(linetable::generate_linetable, submod)?)?;
 
     Ok(submod)
 }
