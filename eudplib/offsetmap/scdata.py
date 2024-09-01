225e6d9392282d6fb459186deb1b144950afc096
Armoha <kein0011@naver.com>
2024-09-02 08:49:36 +0900

    chore: ignore mixed case member name lint

--- eudplib/offsetmap/scdata.py
+++ eudplib/offsetmap/scdata.py
@@ -5,6 +5,7 @@
 # and is released under "MIT License Agreement". Please see the LICENSE
 # file that should have been included as part of this package.
 
+# ruff: noqa: N815
 from .. import utils as ut
 from ..core.rawtrigger.constenc import EncodePlayer, PlayerDict, _Player
 from ..core.rawtrigger.consttype import ConstType
@@ -34,17 +35,17 @@ class TrgPlayer(_Player, EPDOffsetMap):
     __slots__ = ()
     mineral = ore = ArrayMember(0x57F0F0, Mk.DWORD)
     gas = ArrayMember(0x57F120, Mk.DWORD)
-    cumulativeGas = ArrayMember(0x57F150, Mk.DWORD)  # noqa: N815
-    cumulativeMineral = cumulativeOre = ArrayMember(0x57F180, Mk.DWORD)  # noqa: N815
-    zergControlAvailable = ArrayMember(0x582144, Mk.DWORD)  # noqa: N815
-    zergControlUsed = ArrayMember(0x582174, Mk.DWORD)  # noqa: N815
-    zergControlMax = ArrayMember(0x5821A4, Mk.DWORD)  # noqa: N815
-    terranSupplyAvailable = ArrayMember(0x5821D4, Mk.DWORD)  # noqa: N815
-    terranSupplyUsed = ArrayMember(0x582204, Mk.DWORD)  # noqa: N815
-    terranSupplyMax = ArrayMember(0x582234, Mk.DWORD)  # noqa: N815
-    protossPsiAvailable = ArrayMember(0x582264, Mk.DWORD)  # noqa: N815
-    protossPsiUsed = ArrayMember(0x582294, Mk.DWORD)  # noqa: N815
-    protossPsiMax = ArrayMember(0x5822C4, Mk.DWORD)  # noqa: N815
+    cumulativeGas = ArrayMember(0x57F150, Mk.DWORD)
+    cumulativeMineral = cumulativeOre = ArrayMember(0x57F180, Mk.DWORD)
+    zergControlAvailable = ArrayMember(0x582144, Mk.DWORD)
+    zergControlUsed = ArrayMember(0x582174, Mk.DWORD)
+    zergControlMax = ArrayMember(0x5821A4, Mk.DWORD)
+    terranSupplyAvailable = ArrayMember(0x5821D4, Mk.DWORD)
+    terranSupplyUsed = ArrayMember(0x582204, Mk.DWORD)
+    terranSupplyMax = ArrayMember(0x582234, Mk.DWORD)
+    protossPsiAvailable = ArrayMember(0x582264, Mk.DWORD)
+    protossPsiUsed = ArrayMember(0x582294, Mk.DWORD)
+    protossPsiMax = ArrayMember(0x5822C4, Mk.DWORD)
 
     @classmethod
     def cast(cls, other):
@@ -89,66 +90,66 @@ PlayerDict.update({
 class TrgUnit(ConstType, EPDOffsetMap):
     __slots__ = ()
     graphic = flingy = ArrayMember(0x6644F8, Mk.FLINGY)
-    subUnit = ArrayMember(0x6607C0, Mk.UNIT)  # noqa: N815
+    subUnit = ArrayMember(0x6607C0, Mk.UNIT)
     # subunit2 is unused
     # subunit2 = ArrayMember(0x660C38, Mk.WORD)
     # infestationUnit is not implemented yet. (different beginning index)
     # SCBW_DATA(u16*,		InfestedUnitPartial,	unitsDat[3].address);
     # 0x664980, (Id - UnitId::TerranCommandCenter) for it to work,
     # last valid id is UnitId::Special_OvermindCocoon
-    constructionGraphic = ArrayMember(0x6610B0, Mk.BYTE)  # noqa: N815
-    startDirection = ArrayMember(0x6605F0, Mk.BYTE)  # noqa: N815
-    hasShield = ArrayMember(0x6647B0, Mk.BOOL)  # noqa: N815
-    maxShield = ArrayMember(0x660E00, Mk.WORD)  # noqa: N815
-    maxHp = ArrayMember(0x662350, Mk.DWORD)  # noqa: N815
+    constructionGraphic = ArrayMember(0x6610B0, Mk.BYTE)
+    startDirection = ArrayMember(0x6605F0, Mk.BYTE)
+    hasShield = ArrayMember(0x6647B0, Mk.BOOL)
+    maxShield = ArrayMember(0x660E00, Mk.WORD)
+    maxHp = ArrayMember(0x662350, Mk.DWORD)
     elevation = ArrayMember(0x663150, Mk.BYTE)
-    movementFlags = ArrayMember(0x660FC8, Mk.BYTE)  # noqa: N815
+    movementFlags = ArrayMember(0x660FC8, Mk.BYTE)
     rank = ArrayMember(0x663DD0, Mk.BYTE)
-    computerIdleOrder = ArrayMember(0x662EA0, Mk.UNIT_ORDER)  # noqa: N815
-    humanIdleOrder = ArrayMember(0x662268, Mk.UNIT_ORDER)  # noqa: N815
-    returnToIdleOrder = ArrayMember(0x664898, Mk.UNIT_ORDER)  # noqa: N815
-    attackUnitOrder = ArrayMember(0x663320, Mk.UNIT_ORDER)  # noqa: N815
-    attackMoveOrder = ArrayMember(0x663A50, Mk.UNIT_ORDER)  # noqa: N815
-    groundWeapon = ArrayMember(0x6636B8, Mk.WEAPON)  # noqa: N815
-    maxGroundHits = ArrayMember(0x6645E0, Mk.BYTE)  # noqa: N815
-    airWeapon = ArrayMember(0x6616E0, Mk.WEAPON)  # noqa: N815
-    maxAirHits = ArrayMember(0x65FC18, Mk.BYTE)  # noqa: N815
+    computerIdleOrder = ArrayMember(0x662EA0, Mk.UNIT_ORDER)
+    humanIdleOrder = ArrayMember(0x662268, Mk.UNIT_ORDER)
+    returnToIdleOrder = ArrayMember(0x664898, Mk.UNIT_ORDER)
+    attackUnitOrder = ArrayMember(0x663320, Mk.UNIT_ORDER)
+    attackMoveOrder = ArrayMember(0x663A50, Mk.UNIT_ORDER)
+    groundWeapon = ArrayMember(0x6636B8, Mk.WEAPON)
+    maxGroundHits = ArrayMember(0x6645E0, Mk.BYTE)
+    airWeapon = ArrayMember(0x6616E0, Mk.WEAPON)
+    maxAirHits = ArrayMember(0x65FC18, Mk.BYTE)
     # FIXME: split 2 flags into separate members
     AIFlags = ArrayMember(0x660178, Mk.BYTE)
-    baseProperty = ArrayMember(0x664080, Mk.DWORD)  # noqa: N815
-    seekRange = ArrayMember(0x662DB8, Mk.BYTE)  # noqa: N815
-    sightRange = ArrayMember(0x663238, Mk.BYTE)  # noqa: N815
-    armorUpgrade = ArrayMember(0x6635D0, Mk.BYTE)  # noqa: N815
-    sizeType = ArrayMember(0x662180, Mk.BYTE)  # noqa: N815
+    baseProperty = ArrayMember(0x664080, Mk.DWORD)
+    seekRange = ArrayMember(0x662DB8, Mk.BYTE)
+    sightRange = ArrayMember(0x663238, Mk.BYTE)
+    armorUpgrade = ArrayMember(0x6635D0, Mk.BYTE)
+    sizeType = ArrayMember(0x662180, Mk.BYTE)
     armor = ArrayMember(0x65FEC8, Mk.BYTE)
-    rightClickAction = ArrayMember(0x662098, Mk.BYTE)  # noqa: N815
-    readySound = ArrayMember(0x661FC0, Mk.WORD)  # noqa: N815
-    whatSoundStart = ArrayMember(0x65FFB0, Mk.WORD)  # noqa: N815
-    whatSoundEnd = ArrayMember(0x662BF0, Mk.WORD)  # noqa: N815
-    pissedSoundStart = ArrayMember(0x663B38, Mk.WORD)  # noqa: N815
-    pissedSoundEnd = ArrayMember(0x661EE8, Mk.WORD)  # noqa: N815
-    yesSoundStart = ArrayMember(0x663C10, Mk.WORD)  # noqa: N815
-    yesSoundEnd = ArrayMember(0x661440, Mk.WORD)  # noqa: N815
-    buildingDimensions = ArrayMember(0x662860, Mk.POSITION)  # noqa: N815
+    rightClickAction = ArrayMember(0x662098, Mk.BYTE)
+    readySound = ArrayMember(0x661FC0, Mk.WORD)
+    whatSoundStart = ArrayMember(0x65FFB0, Mk.WORD)
+    whatSoundEnd = ArrayMember(0x662BF0, Mk.WORD)
+    pissedSoundStart = ArrayMember(0x663B38, Mk.WORD)
+    pissedSoundEnd = ArrayMember(0x661EE8, Mk.WORD)
+    yesSoundStart = ArrayMember(0x663C10, Mk.WORD)
+    yesSoundEnd = ArrayMember(0x661440, Mk.WORD)
+    buildingDimensions = ArrayMember(0x662860, Mk.POSITION)
     # AddonPlacement is not implemented yet because its beginning index isn't 0.
     # addonPlacement = ArrayMember(0x6626E0, Mk.POSITION)
     # unitDimensions is not implemented yet.
     # unitBoundsLURB = ArrayMember(0x6617C8, 2 * Mk.POSITION)
     portrait = ArrayMember(0x662F88, Mk.WORD)
-    mineralCost = ArrayMember(0x663888, Mk.WORD)  # noqa: N815
-    gasCost = ArrayMember(0x65FD00, Mk.WORD)  # noqa: N815
-    timeCost = ArrayMember(0x660428, Mk.WORD)  # noqa: N815
-    requirementOffset = ArrayMember(0x660A70, Mk.WORD)  # noqa: N815
-    groupFlags = ArrayMember(0x6637A0, Mk.BYTE)  # noqa: N815
-    supplyProvided = ArrayMember(0x6646C8, Mk.BYTE)  # noqa: N815
-    supplyUsed = ArrayMember(0x663CE8, Mk.BYTE)  # noqa: N815
-    transportSpaceProvided = ArrayMember(0x660988, Mk.BYTE)  # noqa: N815
-    transportSpaceRequired = ArrayMember(0x664410, Mk.BYTE)  # noqa: N815
-    buildScore = ArrayMember(0x663408, Mk.WORD)  # noqa: N815
-    killScore = ArrayMember(0x663EB8, Mk.WORD)  # noqa: N815
-    nameString = ArrayMember(0x660260, Mk.WORD)  # noqa: N815
-    broodWarFlag = ArrayMember(0x6606D8, Mk.BYTE)  # noqa: N815
-    stareditAvailabilityFlags = ArrayMember(0x661528, Mk.WORD)  # noqa: N815
+    mineralCost = ArrayMember(0x663888, Mk.WORD)
+    gasCost = ArrayMember(0x65FD00, Mk.WORD)
+    timeCost = ArrayMember(0x660428, Mk.WORD)
+    requirementOffset = ArrayMember(0x660A70, Mk.WORD)
+    groupFlags = ArrayMember(0x6637A0, Mk.BYTE)
+    supplyProvided = ArrayMember(0x6646C8, Mk.BYTE)
+    supplyUsed = ArrayMember(0x663CE8, Mk.BYTE)
+    transportSpaceProvided = ArrayMember(0x660988, Mk.BYTE)
+    transportSpaceRequired = ArrayMember(0x664410, Mk.BYTE)
+    buildScore = ArrayMember(0x663408, Mk.WORD)
+    killScore = ArrayMember(0x663EB8, Mk.WORD)
+    nameString = ArrayMember(0x660260, Mk.WORD)
+    broodWarFlag = ArrayMember(0x6606D8, Mk.BYTE)
+    stareditAvailabilityFlags = ArrayMember(0x661528, Mk.WORD)
 
     @classmethod
     def cast(cls, s):
@@ -169,28 +170,28 @@ class Weapon(ConstType, EPDOffsetMap):
     graphic = flingy = ArrayMember(0x656CA8, Mk.FLINGY)
     # special attack is for reference only?
     # specialAttack = ArrayMember(0x6573E8, Mk.BYTE)
-    targetFlags = ArrayMember(0x657998, Mk.DWORD)  # noqa: N815
+    targetFlags = ArrayMember(0x657998, Mk.DWORD)
     # can't use range because it's a python keyword
-    minRange = ArrayMember(0x656A18, Mk.DWORD)  # noqa: N815
-    maxRange = ArrayMember(0x6573E8, Mk.DWORD)  # noqa: N815
-    upgrade = upgradeID = ArrayMember(0x6571D0, Mk.BYTE)  # noqa: N815
-    damageType = ArrayMember(0x657258, Mk.BYTE)  # noqa: N815
+    minRange = ArrayMember(0x656A18, Mk.DWORD)
+    maxRange = ArrayMember(0x6573E8, Mk.DWORD)
+    upgrade = upgradeID = ArrayMember(0x6571D0, Mk.BYTE)
+    damageType = ArrayMember(0x657258, Mk.BYTE)
     # Fly and follow target, appear on target unit, etc.
     behavior = ArrayMember(0x656670, Mk.BYTE)
-    removeAfter = ArrayMember(0x657040, Mk.BYTE)  # noqa: N815
-    explosionType = ArrayMember(0x6566F8, Mk.BYTE)  # noqa: N815
-    splashInnerRadius = ArrayMember(0x656888, Mk.WORD)  # noqa: N815
-    splashMiddleRadius = ArrayMember(0x6570C8, Mk.WORD)  # noqa: N815
-    splashOuterRadius = ArrayMember(0x657780, Mk.WORD)  # noqa: N815
+    removeAfter = ArrayMember(0x657040, Mk.BYTE)
+    explosionType = ArrayMember(0x6566F8, Mk.BYTE)
+    splashInnerRadius = ArrayMember(0x656888, Mk.WORD)
+    splashMiddleRadius = ArrayMember(0x6570C8, Mk.WORD)
+    splashOuterRadius = ArrayMember(0x657780, Mk.WORD)
     damage = ArrayMember(0x656EB0, Mk.WORD)
-    damageBonus = ArrayMember(0x657678, Mk.WORD)  # noqa: N815
+    damageBonus = ArrayMember(0x657678, Mk.WORD)
     cooldown = ArrayMember(0x656EBC, Mk.BYTE)
-    damageFactor = ArrayMember(0x6564E0, Mk.BYTE)  # noqa: N815
-    attackAngle = ArrayMember(0x656990, Mk.BYTE)  # noqa: N815
-    launchSpin = ArrayMember(0x657888, Mk.BYTE)  # noqa: N815
-    forwardOffset = graphicXOffset = ArrayMember(0x657910, Mk.BYTE)  # noqa: N815
-    verticalOffset = graphicYOffset = ArrayMember(0x656C20, Mk.BYTE)  # noqa: N815
-    targetErrorMessage = ArrayMember(0x656568, Mk.WORD)  # noqa: N815
+    damageFactor = ArrayMember(0x6564E0, Mk.BYTE)
+    attackAngle = ArrayMember(0x656990, Mk.BYTE)
+    launchSpin = ArrayMember(0x657888, Mk.BYTE)
+    forwardOffset = graphicXOffset = ArrayMember(0x657910, Mk.BYTE)
+    verticalOffset = graphicYOffset = ArrayMember(0x656C20, Mk.BYTE)
+    targetErrorMessage = ArrayMember(0x656568, Mk.WORD)
     icon = ArrayMember(0x656780, Mk.WORD)
 
     @classmethod
@@ -208,13 +209,13 @@ class Weapon(ConstType, EPDOffsetMap):
 
 class Flingy(ConstType, EPDOffsetMap):
     __slots__ = ()
-    sprite = spriteID = ArrayMember(0x6CA318, Mk.SPRITE)  # noqa: N815
-    topSpeed = ArrayMember(0x6C9EF8, Mk.DWORD)  # noqa: N815
+    sprite = spriteID = ArrayMember(0x6CA318, Mk.SPRITE)
+    topSpeed = ArrayMember(0x6C9EF8, Mk.DWORD)
     acceleration = ArrayMember(0x6C9C78, Mk.WORD)
-    haltDistance = ArrayMember(0x6C9930, Mk.DWORD)  # noqa: N815
-    turnSpeed = turnRadius = ArrayMember(0x6C9E20, Mk.BYTE)  # noqa: N815
+    haltDistance = ArrayMember(0x6C9930, Mk.DWORD)
+    turnSpeed = turnRadius = ArrayMember(0x6C9E20, Mk.BYTE)
     # unused = ArrayMember(0x6CA240, Mk.BYTE)
-    movementControl = ArrayMember(0x6C9858, Mk.BYTE)  # noqa: N815
+    movementControl = ArrayMember(0x6C9858, Mk.BYTE)
 
     @classmethod
     def cast(cls, s):
@@ -232,10 +233,10 @@ class Flingy(ConstType, EPDOffsetMap):
 class Sprite(ConstType, EPDOffsetMap):
     __slots__ = ()
     # Read only data skipped
-    image = imageID = ArrayMember(0x666160, Mk.IMAGE)  # noqa: N815
+    image = imageID = ArrayMember(0x666160, Mk.IMAGE)
     # hpBarSize = ArrayMember(0x665E50, Mk.BYTE)
     # ??? = ArrayMember(0x666570, Mk.BYTE)
-    isVisible = ArrayMember(0x665C48, Mk.BOOL)  # noqa: N815
+    isVisible = ArrayMember(0x665C48, Mk.BOOL)
     # selectionCircle = ArrayMember(0x665AC0, Mk.BYTE)
     # selectionVerticalOffset = ArrayMember(0x665FD8, Mk.BYTE)
 
@@ -256,15 +257,15 @@ class Image(ConstType, EPDOffsetMap):
     __slots__ = ()
     # Read only data skipped
     # grpFile = ArrayMember(0x668AA0, Mk.DWORD)
-    isTurnable = graphicTurn = ArrayMember(0x66E860, Mk.BOOL)  # noqa: N815
-    isClickable = ArrayMember(0x66C150, Mk.BOOL)  # noqa: N815
-    useFullIscript = ArrayMember(0x66D4D8, Mk.BOOL)  # noqa: N815
-    drawIfCloaked = ArrayMember(0x667718, Mk.BOOL)  # noqa: N815
-    drawingFunction = ArrayMember(0x669E28, Mk.BYTE)  # noqa: N815
+    isTurnable = graphicTurn = ArrayMember(0x66E860, Mk.BOOL)
+    isClickable = ArrayMember(0x66C150, Mk.BOOL)
+    useFullIscript = ArrayMember(0x66D4D8, Mk.BOOL)
+    drawIfCloaked = ArrayMember(0x667718, Mk.BOOL)
+    drawingFunction = ArrayMember(0x669E28, Mk.BYTE)
     # Remapping table is skipped because it doesn't work in SC:R
     # FIXME: Add UnsupportedMember
     # remapping = ArrayMember(0x669A40, Mk.BYTE)
-    iscript = iscriptID = ArrayMember(0x66EC48, Mk.DWORD)  # noqa: N815
+    iscript = iscriptID = ArrayMember(0x66EC48, Mk.DWORD)
     # shieldsOverlay = ArrayMember(0x66C538, Mk.DWORD)
     # attackOverlay = ArrayMember(0x66B1B0, Mk.DWORD)
     # damageOverlay = ArrayMember(0x66A210, Mk.DWORD)
@@ -287,18 +288,18 @@ class Image(ConstType, EPDOffsetMap):
 
 class Upgrade(ConstType, EPDOffsetMap):
     __slots__ = ()
-    mineralCostBase = ArrayMember(0x656740, Mk.WORD)  # noqa: N815
-    mineralCostFactor = ArrayMember(0x6559C0, Mk.WORD)  # noqa: N815
-    gasCostBase = ArrayMember(0x655840, Mk.WORD)  # noqa: N815
-    gasCostFactor = ArrayMember(0x6557C0, Mk.WORD)  # noqa: N815
-    timeCostBase = ArrayMember(0x655B80, Mk.WORD)  # noqa: N815
-    timeCostFactor = ArrayMember(0x655940, Mk.WORD)  # noqa: N815
-    requirementOffset = ArrayMember(0x6558C0, Mk.WORD)  # noqa: N815
+    mineralCostBase = ArrayMember(0x656740, Mk.WORD)
+    mineralCostFactor = ArrayMember(0x6559C0, Mk.WORD)
+    gasCostBase = ArrayMember(0x655840, Mk.WORD)
+    gasCostFactor = ArrayMember(0x6557C0, Mk.WORD)
+    timeCostBase = ArrayMember(0x655B80, Mk.WORD)
+    timeCostFactor = ArrayMember(0x655940, Mk.WORD)
+    requirementOffset = ArrayMember(0x6558C0, Mk.WORD)
     icon = ArrayMember(0x655AC0, Mk.WORD)
     label = ArrayMember(0x655A40, Mk.WORD)
     race = ArrayMember(0x655BFC, Mk.BYTE)
-    maxLevel = ArrayMember(0x655700, Mk.BYTE)  # noqa: N815
-    broodWarFlag = ArrayMember(0x655B3C, Mk.BYTE)  # noqa: N815
+    maxLevel = ArrayMember(0x655700, Mk.BYTE)
+    broodWarFlag = ArrayMember(0x655B3C, Mk.BYTE)
 
     @classmethod
     def cast(cls, s):
@@ -315,17 +316,17 @@ class Upgrade(ConstType, EPDOffsetMap):
 
 class Tech(ConstType, EPDOffsetMap):
     __slots__ = ()
-    mineralCost = ArrayMember(0x656248, Mk.WORD)  # noqa: N815
-    gasCost = ArrayMember(0x6561F0, Mk.WORD)  # noqa: N815
-    timeCost = ArrayMember(0x6563D8, Mk.WORD)  # noqa: N815
-    energyCost = ArrayMember(0x656380, Mk.WORD)  # noqa: N815
+    mineralCost = ArrayMember(0x656248, Mk.WORD)
+    gasCost = ArrayMember(0x6561F0, Mk.WORD)
+    timeCost = ArrayMember(0x6563D8, Mk.WORD)
+    energyCost = ArrayMember(0x656380, Mk.WORD)
     # ??? = ArrayMember(0x656198, Mk.WORD)
-    requirementOffset = ArrayMember(0x6562F8, Mk.WORD)  # noqa: N815
+    requirementOffset = ArrayMember(0x6562F8, Mk.WORD)
     icon = ArrayMember(0x656430, Mk.WORD)
     label = ArrayMember(0x6562A0, Mk.WORD)
     race = ArrayMember(0x656488, Mk.BYTE)
     researched = ArrayMember(0x656350, Mk.BYTE)  # UNUSED?
-    broodWarFlag = ArrayMember(0x6564B4, Mk.BYTE)  # noqa: N815
+    broodWarFlag = ArrayMember(0x6564B4, Mk.BYTE)
 
     @classmethod
     def cast(cls, s):
@@ -343,24 +344,24 @@ class Tech(ConstType, EPDOffsetMap):
 class UnitOrder(ConstType, EPDOffsetMap):
     __slots__ = ()
     label = ArrayMember(0x665280, Mk.WORD)
-    useWeaponTargeting = ArrayMember(0x664B00, Mk.BOOL)  # noqa: N815
+    useWeaponTargeting = ArrayMember(0x664B00, Mk.BOOL)
     # secondaryOrder = ArrayMember(0x665940, Mk.BYTE)
     # nonSubUnit = ArrayMember(0x665A00, Mk.BYTE)
     # ??? = ArrayMember(0x664A40, Mk.BYTE)
     # subUnitcanUse = ArrayMember(0x6657C0, Mk.BYTE)
-    canBeInterrupted = ArrayMember(0x665040, Mk.BOOL)  # noqa: N815
+    canBeInterrupted = ArrayMember(0x665040, Mk.BOOL)
     # ??? = ArrayMember(0x665100, Mk.BYTE)
-    canBeQueued = ArrayMember(0x665700, Mk.BOOL)  # noqa: N815
+    canBeQueued = ArrayMember(0x665700, Mk.BOOL)
     # ??? = ArrayMember(0x6651C0, Mk.BYTE)
-    canBeObstructed = ArrayMember(0x6654C0, Mk.BOOL)  # noqa: N815
+    canBeObstructed = ArrayMember(0x6654C0, Mk.BOOL)
     # ??? = ArrayMember(0x664C80, Mk.BYTE)
     # requireMoving = ArrayMember(0x664BC0, Mk.BYTE)
-    weapon = weaponID = ArrayMember(0x665880, Mk.BYTE)  # noqa: N815
-    techUsed = ArrayMember(0x664E00, Mk.BYTE)  # noqa: N815
+    weapon = weaponID = ArrayMember(0x665880, Mk.BYTE)
+    techUsed = ArrayMember(0x664E00, Mk.BYTE)
     animation = ArrayMember(0x664D40, Mk.BYTE)
-    buttonIcon = ArrayMember(0x664EC0, Mk.WORD)  # noqa: N815
-    requirementOffset = ArrayMember(0x665580, Mk.WORD)  # noqa: N815
-    obscuredOrder = ArrayMember(0x665400, Mk.BYTE)  # noqa: N815
+    buttonIcon = ArrayMember(0x664EC0, Mk.WORD)
+    requirementOffset = ArrayMember(0x665580, Mk.WORD)
+    obscuredOrder = ArrayMember(0x665400, Mk.BYTE)
 
     @classmethod
     def cast(cls, s):
