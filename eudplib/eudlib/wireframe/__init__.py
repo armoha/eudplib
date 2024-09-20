090507e1770f597bb2e26727759612dc3f9278e5
Armoha <kein0011@naver.com>
2024-09-20 22:43:16 +0900

    add license text to source files

---
+++ eudplib/eudlib/wireframe/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2022 by Armoha.
+# All rights reserved.
+# This file is part of EUD python library (eudplib),
+# and is released under "MIT License Agreement". Please see the LICENSE
+# file that should have been included as part of this package.
--- eudplib/maprw/injector/payload_init.py
+++ eudplib/maprw/injector/payload_init.py
@@ -1,3 +1,9 @@
+# Copyright 2020 by Armoha.
+# All rights reserved.
+# This file is part of EUD python library (eudplib),
+# and is released under "MIT License Agreement". Please see the LICENSE
+# file that should have been included as part of this package.
+
 from collections.abc import ByteString
 
 from ... import utils as ut
