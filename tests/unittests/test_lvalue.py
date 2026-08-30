47d284f3921a0dac0722436e265ef74c0f87a1cd
Armoha <kein0011@naver.com>
2026-08-30 18:14:34 +0900

    refactor: drop refcount-based ad-hoc rvalue optimization
    It doesn't work on latest pyo3 or Python 3.14. epScript compiler should do control flow analysis and data flow analysis to properly implement return value optimization in future.

--- tests/unittests/test_lvalue.py
+++ tests/unittests/test_lvalue.py
@@ -20,7 +20,6 @@ def test_function_lvalue():
 
 @TestInstance
 def test_rvalue():
-    import sys
     from eudplib.core.variable.evcommon import _addor
 
     v1 = EUDVariable(0)
@@ -28,64 +27,56 @@ def test_rvalue():
     trg = NextTrigger()
     trg_count = GetTriggerCounter()
     v2 = v1 + 1 + EUDVariable(2) + 3 + EUDVariable(4) + 5
-    if sys.version_info < (3, 14):
-        ep_assert(trg_count + 5 == GetTriggerCounter(), "Failed to optimize rvalue")
-        test_equality(
-            "rvalue reuse test: self, __add__",
-            [v2, f_dwread(trg + 344)],
-            [15, EPD(v2.getValueAddr())],
-        )
+    ep_assert(trg_count + 5 == GetTriggerCounter(), "No copy elision, TODO")
+    test_equality(
+        "rvalue reuse test: self, __add__",
+        [v2, f_dwread(trg + 344)],
+        [15, EPD(v2.getValueAddr())],
+    )
 
     trg = NextTrigger()
     v3 = v2 + EUDVariable(6)
-    if sys.version_info < (3, 14):
-        test_equality(
-            "rvalue reuse test: other, __add__",
-            [v3, f_dwread(trg + 4), f_dwread(trg + 344), f_dwread(trg + 348)],
-            [21, v2.GetVTable(), EPD(v2.getDestAddr()), EPD(v3.getValueAddr())],
-        )
+    test_equality(
+        "rvalue reuse test: other, __add__",
+        [v3, f_dwread(trg + 4), f_dwread(trg + 344), f_dwread(trg + 348)],
+        [21, v2.GetVTable(), EPD(v2.getDestAddr()), EPD(v3.getValueAddr())],
+    )
 
     trg = NextTrigger()
     trg_count = GetTriggerCounter()
     v4 = EUDVariable(20) - v2
-    if sys.version_info < (3, 14):
-        ep_assert(
-            trg_count + 1 == GetTriggerCounter(),
-            f"{trg_count} + 1 != {GetTriggerCounter()}",
-        )
-        nptr = f_dwread(v2.GetVTable() + 4)
-        test_equality(
-            "rvalue reuse test: self, __sub__",
-            [v4, nptr, f_dwread(trg + 344), f_dwread(trg + 348), f_dwread(trg + 4)],
-            [
-                5,
-                _addor.GetVTable(),
-                EPD(v4.getValueAddr()),
-                1,
-                v2.GetVTable(),
-            ],
-        )
+    ep_assert(
+        trg_count + 2 == GetTriggerCounter(),
+        f"{trg_count} + 2 != {GetTriggerCounter()}",
+    )
+    nptr = f_dwread(v2.GetVTable() + 4)
+    test_equality(
+        "rvalue reuse test: self, __sub__",
+        [v4, nptr, f_dwread(trg + 344), f_dwread(trg + 348), f_dwread(trg + 4)],
+        [
+            5,
+            _addor.GetVTable(),
+            EPD(v4.getValueAddr()),
+            1,
+            v2.GetVTable(),
+        ],
+    )
 
     trg = NextTrigger()
     trg_count = GetTriggerCounter()
     v5 = v3 - EUDVariable(9)
-    if sys.version_info < (3, 14):
-        ep_assert(
-            trg_count + 1 == GetTriggerCounter(),
-            f"{trg_count} + 1 != {GetTriggerCounter()}",
-        )
-        test_equality(
-            "rvalue reuse test: other, __sub__",
-            [
-                v5,
-                f_dwread(trg + 4),
-                f_dwread(trg + 328),
-                f_dwread(trg + 344),
-                f_dwread(trg + 348),
-            ],
-            [12, v3.GetVTable(), 0x55555555, EPD(v5.getValueAddr()), 0xFFFFFFFF],
-        )
-
-        # this test is for _yield_and_check_rvalue(), not checkNonRValue()
-        with expect_eperror():
-            SetVariables(f_dwread_epd(0), 0)
+    ep_assert(
+        trg_count + 2 == GetTriggerCounter(),
+        f"{trg_count} + 2 != {GetTriggerCounter()}",
+    )
+    test_equality(
+        "rvalue reuse test: other, __sub__",
+        [
+            v5,
+            f_dwread(trg + 4),
+            f_dwread(trg + 328),
+            f_dwread(trg + 344),
+            f_dwread(trg + 348),
+        ],
+        [12, v3.GetVTable(), 0x55555555, EPD(v5.getValueAddr()), 0xFFFFFFFF],
+    )
